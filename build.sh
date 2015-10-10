0e3e1d8af963598b27438133dbf59c8770735186
a1batross <a1ba.omarov@gmail.com>
2015-10-10 22:37:58 +0300

    Remove unused line

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=0 V=0 XASH_SDL=1 -j8 APP_CFLAGS="-
 sh gen-version.sh default-release
 ant release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../myks.keystore bin/xashdroid-release-unsigned.apk xashdroid
-#adb install -r -f bin/xashdroid-debug.apk
 rm bin/xashdroid-release.apk
 /home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/xashdroid-release-unsigned.apk bin/xashdroid-release.apk
