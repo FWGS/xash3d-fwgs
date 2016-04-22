d1e8dce81a3157b2d6847286e7415d3b47fd87c2
mittorn <mittorn@sibmail.com>
2016-04-22 19:02:06 +0000

    Experimental opm support

--- jni/Application.mk
+++ jni/Application.mk
@@ -11,11 +11,13 @@ else
 APP_PLATFORM := android-8
 endif
 
+# if non-zero, works only if single ABI selected
+XASH_THREAD_NUM ?= 0
 
-CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=1024
-CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -mvectorize-with-neon-quad -DVECTORIZE_SINCOS -fPIC
-CFLAGS_OPT_ARMv5 :=-march=armv6 -mfpu=vfp -marm -pipe
-CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS
+CFLAGS_OPT :=  -O3 -fno-omit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=512 -ftree-loop-distribution -fprefetch-loop-arrays -floop-strip-mine -ftree-loop-if-convert -ftree-loop-distribution -pipe
+CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -mvectorize-with-neon-quad -DVECTORIZE_SINCOS -fPIC
+CFLAGS_OPT_ARMv5 :=-march=armv6 -mfpu=vfp -marm
+CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -DVECTORIZE_SINCOS
 CFLAGS_HARDFP := -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard -DLOAD_HARDFP -DSOFTFP_LINK
 APPLICATIONMK_PATH = $(call my-dir)
 
