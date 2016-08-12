8d26578c332f84bb0962c0888fd4c93101d91d9f
mittorn <mittorn@sibmail.com>
2016-08-12 21:40:08 +0000

    Update test build script

--- build-test.sh
+++ build-test.sh
@@ -17,7 +17,8 @@ sh gen-version.sh test build
 rm assets/extras.pak
 python2.7 makepak.py xash-extras assets/extras.pak
 $AAPT package -m -J gen/ --rename-manifest-package in.celest.xash3d.hl -M AndroidManifest.xml -S test/res -I $ANDROID_JAR
-$JAVAC -d bin/classes -s bin/classes -cp $ANDROID_JAR gen/in/celest/xash3d/hl/R.java src/in/celest/xash3d/*.java
+echo "package in.celest.xash3d.hl;public final class BuildConfig {public final static boolean DEBUG = true;}" > gen/in/celest/xash3d/hl/BuildConfig.java
+$JAVAC -d bin/classes -s bin/classes -cp $ANDROID_JAR gen/in/celest/xash3d/hl/*  src/in/celest/xash3d/*.java
 $DX --dex --output=bin/classes.dex bin/classes/
 $AAPT package -f -M test/AndroidManifest.xml -S test/res -I $ANDROID_JAR -F bin/xash3d.apk.unaligned
 zip bin/xash3d.apk.unaligned assets/*
