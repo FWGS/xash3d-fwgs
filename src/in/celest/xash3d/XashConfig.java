e759bf819a56e5400ef2bb87fb8997afe384314a
mittorn <mittorn@sibmail.com>
2016-09-14 09:16:34 +0000

    Ant travis test build

---
+++ src/in/celest/xash3d/XashConfig.java
@@ -0,0 +1,7 @@
+package in.celest.xash3d;
+public class XashConfig {
+public static final boolean PKG_TEST = false;
+public static final boolean CHECK_SIGNATURES = false;
+public static final boolean RELEASE = false;
+public static final boolean GP_VERSION = false;
+}
