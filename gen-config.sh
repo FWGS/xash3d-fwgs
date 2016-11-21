41ba97d426d9c41e5502d147810303c8f4018fe4
mittorn <mittorn@sibmail.com>
2016-11-22 02:01:10 +0600

    Fix gen-config

--- gen-config.sh
+++ gen-config.sh
@@ -2,17 +2,21 @@
 
 git update-index --assume-unchanged src/in/celest/xash3d/XashConfig.java
 PKG_TEST=false
-if [[ $* =~ "test" ]] ; then  export PKG_TEST=true; fi
-
 CHECK_SIGNATURES=false
-if [[ $* =~ "sign" ]] ; then  export CHECK_SIGNATURES=true; fi
-
 RELEASE=false
-if [[ $* =~ "release" ]] ; then  export RELEASE=true; fi
-
 GP_VERSION=false
-if [[ $* =~ "gp" ]] ; then  export GP_VERSION=true; fi
-
+case "$*" in
+*test*) export PKG_TEST=true;;
+esac
+case "$*" in
+*sign*) export CHECK_SIGNATURES=true;
+esac
+case "$*" in
+*release*) export RELEASE=true;;
+esac
+case "$*" in
+*gp*) export GP_VERSION=true;;
+esac
 _V="public static final boolean"
 echo package in.celest.xash3d\; >src/in/celest/xash3d/XashConfig.java
 echo public class XashConfig { >>src/in/celest/xash3d/XashConfig.java
