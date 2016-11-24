a38603ebe189f0dfe2f2af4c45dc56cf0116507a
mittorn <mittorn@sibmail.com>
2016-11-25 00:58:27 +0600

    Add github deploy script

---
+++ travis-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+ echo "Travis should not deploy from pull requests"
+ exit 0
+else
+ mkdir xash3d-travis
+ cp -a $* xash3d-travis/
+ cd xash3d-travis
+ git init
+ git config user.name mittorn
+ git config user.email mittorn@sibmail.com
+ git remote add travis-deploy-public https://mittorn:${GH_TOKEN}@github.com/mittorn/xash3d-travis.git
+ git add .
+ git commit -m "Laterst travis android optimized deploy $TRAVIS_COMMIT"
+ git checkout -b android-optimized-$TRAVIS_BRANCH
+ git push --force travis-deploy-public android-optimized-$TRAVIS_BRANCH
+fi
