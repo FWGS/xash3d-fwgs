2e9b4bf07c85acbf8a96868f015af23f6f1e9b22
mittorn <mittorn@sibmail.com>
2016-11-24 12:36:00 +0600

    Try new upload script

---
+++ travis-upload.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Upload travis generated APKs to the Transfer.sh and Yandex.Disk
+
+getDaysSinceRelease()
+{
+	printf %04d $(( ( $(date +'%s') - $(date -ud '2015-04-01' +'%s') )/60/60/24 ))
+}
+
+DAYSSINCERELEASE=`getDaysSinceRelease`
+COMMITHASH=$(git rev-parse --short HEAD)
+#CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+
+PREFIX=$DAYSSINCERELEASE-$(date +"%H-%M")
+POSTFIX=$COMMITHASH
+
+while [[ $1 ]]; do
+FNAME=$1
+FILE_BASE=${FNAME%.*}
+FILE_EXT="${FNAME##*.}"
+OUTNAME=$PREFIX-$FILE_BASE-$POSTFIX.$FILE_EXT
+echo $FNAME: `curl --upload-file $FNAME https://transfer.sh/$OUTNAME`
+curl -T $FNAME -u $YADISK_USERNAME:$YADISK_PASSWORD https://webdav.yandex.ru/XashTestVersions/$YADISKPATH/$OUTNAME
+curl -T xashdroid-armv7.apk -u $YADISK_USERNAME:$YADISK_PASSWORD https://webdav.yandex.ru/XashTestVersions/current-$FILE_BASE-$TRAVIS_BRANCH.$FILE_EXT
+shift
+done
+exit 0
