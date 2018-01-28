d7389e45119539dd336f498a92c23b2b84ea3f49
Alibek Omarov <a1ba.omarov@gmail.com>
2018-01-29 01:58:52 +0300

    Remove unneeded SUPPORT_WEBP=false from travis.yml, as we don't use SDL and SDL_image. Update travis-deploy.sh from engine repo

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
  echo "Travis should not deploy from pull requests"
  exit 0
@@ -11,20 +11,18 @@ else
  git init
  git config user.name FWGS-deployer
  git config user.email FWGS-deployer@users.noreply.github.com
- git config credential.helper "store --file=.git/credentials"
- git config credential.https://github.com.com.username FWGS-deployer
- echo "https://FWGS-deployer:${GH_TOKEN}@github.com" > .git/credentials
- git remote add travis-deploy-public https://github.com/FWGS/xash3d-deploy.git
+ git remote add travis-deploy-public https://FWGS-deployer:${GH_TOKEN}@github.com/FWGS/xash3d-deploy.git
  echo \# $TRAVIS_BRANCH branch autobuilds from $SOURCE_NAME >> README.md
  echo >> README.md
  echo Short changelog: >> README.md
  echo \`\`\` >> README.md
- (cd ..;git log --pretty=format:'%h %ad %s' --date iso -n 10 HEAD `curl https://raw.githubusercontent.com/FWGS/xash3d-deploy/$SOURCE_NAME-$TRAVIS_BRANCH/commit.txt`.. )| cut -d ' ' -f 1-3,5-100 >> README.md
+ (cd $TRAVIS_BUILD_DIR;git log --pretty=format:'%h %ad %s' --date iso -n 10 HEAD `curl https://raw.githubusercontent.com/FWGS/xash3d-deploy/$SOURCE_NAME-$TRAVIS_BRANCH/commit.txt`
+.. )| cut -d ' ' -f 1-3,5-100 >> README.md
  echo \`\`\` >> README.md
  echo >> README.md
- echo [code on github]\(https://github.com/FWGS/xash3d-android-project/tree/$TRAVIS_COMMIT\) >> README.md
+ echo [Code on GitHub]\(https://github.com/FWGS/xash3d/tree/$TRAVIS_COMMIT\) >> README.md
  echo >> README.md
- echo [full changelog for this build]\(https://github.com/FWGS/xash3d-android-project/commits/$TRAVIS_COMMIT\) >> README.md
+ echo [Full changelog for this build]\(https://github.com/FWGS/xash3d/commits/$TRAVIS_COMMIT\) >> README.md
  echo >> README.md
  for arg in $*; do
   echo \* [$arg]\(https://github.com/FWGS/xash3d-deploy/blob/$SOURCE_NAME-$TRAVIS_BRANCH/$arg\?raw\=true\) >> README.md
@@ -32,10 +30,10 @@ else
  done
  echo $TRAVIS_COMMIT > commit.txt
  git add .
- git commit -m "Laterst travis deploy $TRAVIS_COMMIT"
+ git commit -m "Latest travis deploy $TRAVIS_COMMIT"
  git checkout -b $SOURCE_NAME-$TRAVIS_BRANCH
- git push -q --force travis-deploy-public $SOURCE_NAME-$TRAVIS_BRANCH
+ git push -q --force travis-deploy-public $SOURCE_NAME-$TRAVIS_BRANCH >/dev/null 2>/dev/null
  git checkout -b $SOURCE_NAME-latest
- git push -q --force travis-deploy-public $SOURCE_NAME-latest
+ git push -q --force travis-deploy-public $SOURCE_NAME-latest >/dev/null 2>/dev/null
 fi
 exit 0
