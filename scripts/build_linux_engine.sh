f9b13472b589b3ca52053ab395a13eeb57e21f7f
Alibek Omarov <a1ba.omarov@gmail.com>
2018-12-12 04:19:17 +0300

    scripts: autobuild appimage on travis

--- scripts/build_linux_engine.sh
+++ scripts/build_linux_engine.sh
@@ -31,6 +31,6 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 ./waf configure --sdl2=$TRAVIS_BUILD_DIR/SDL2_linux --vgui=$TRAVIS_BUILD_DIR/vgui-dev --build-type=debug --use-stb
 ./waf build -j2
-# cp engine/xash3d mainui/libxashmenu.so vgui_support/libvgui_support.so vgui_support/vgui.so ../scripts/xash3d.sh .
-# cp $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/$(readlink $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/libSDL2-2.0.so.0) libSDL2-2.0.so.0
-# 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-linux.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on xash3d libSDL2-2.0.so.0 libvgui_support.so vgui.so libxashmenu.so xash3d.sh
+
+# Build AppImage
+scripts/build_appimage.sh
