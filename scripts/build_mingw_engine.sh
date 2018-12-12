154ef82719da9934fbaa1bb7c58c0f2f7d686d8c
Alibek Omarov <a1ba.omarov@gmail.com>
2018-12-12 04:49:11 +0300

    scripts: add mingw build archive creation

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Build engine
-
 cd $TRAVIS_BUILD_DIR
 export CC="ccache i686-w64-mingw32-gcc"
 export CXX="ccache i686-w64-mingw32-g++"
@@ -10,6 +9,9 @@ export CXXFLAGS="-static-libgcc -static-libstdc++"
 export WINRC="i686-w64-mingw32-windres"
 ./waf configure -o build-mingw --sdl2=$TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32/ --no-vgui --build-type=debug # can't compile VGUI support on MinGW, due to differnet C++ ABI
 ./waf build -o build-mingw -j2
-# cp SDL2/SDL2-2.0.7/i686-w64-mingw32/bin/SDL2.dll . # Install SDL2
-# cp /usr/i686-w64-mingw32/lib/libwinpthread-1.dll . # a1ba: remove when travis will be updated to xenial
-# 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on xash_sdl.exe menu.dll SDL2.dll vgui_support.dll libwinpthread-1.dll
+cp SDL2/SDL2-2.0.7/i686-w64-mingw32/bin/SDL2.dll . # Install SDL2
+cp vgui_support_bin/vgui_support.dll .
+cp build-mingw/engine/xash.dll .
+cp build-mingw/mainui/menu.dll .
+cp build-mingw/game_launch/xash3d.exe .
+7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
