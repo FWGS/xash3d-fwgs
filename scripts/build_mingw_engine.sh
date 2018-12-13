2072f5c2767b99bfe5ca34c3d632976d5e6fc065
Alibek Omarov <a1ba.omarov@gmail.com>
2018-12-13 18:44:13 +0300

    scripts: fix travis scripts after refactoring wscript

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -7,7 +7,7 @@ export CXX="ccache i686-w64-mingw32-g++"
 export CFLAGS="-static-libgcc -no-pthread"
 export CXXFLAGS="-static-libgcc -static-libstdc++"
 export WINRC="i686-w64-mingw32-windres"
-./waf configure -o build-mingw --sdl2=$TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32/ --no-vgui --build-type=debug --verbose # can't compile VGUI support on MinGW, due to differnet C++ ABI
+./waf configure -o build-mingw --sdl2=$TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32/ --disable-vgui --build-type=debug --verbose # can't compile VGUI support on MinGW, due to differnet C++ ABI
 ./waf build -o build-mingw -j2 --verbose
 cp $TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32//bin/SDL2.dll . # Install SDL2
 cp vgui_support_bin/vgui_support.dll .
