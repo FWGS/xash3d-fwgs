6eb327eac271ec3f3d1313189b5cf327dc2b46ba
Alibek Omarov <a1ba.omarov@gmail.com>
2018-12-13 03:25:46 +0300

    scripts: enable verbose build, fix including SDL2

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -7,9 +7,9 @@ export CXX="ccache i686-w64-mingw32-g++"
 export CFLAGS="-static-libgcc -no-pthread"
 export CXXFLAGS="-static-libgcc -static-libstdc++"
 export WINRC="i686-w64-mingw32-windres"
-./waf configure -o build-mingw --sdl2=$TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32/ --no-vgui --build-type=debug # can't compile VGUI support on MinGW, due to differnet C++ ABI
-./waf build -o build-mingw -j2
-cp SDL2/SDL2-2.0.7/i686-w64-mingw32/bin/SDL2.dll . # Install SDL2
+./waf configure -o build-mingw --sdl2=$TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32/ --no-vgui --build-type=debug --verbose # can't compile VGUI support on MinGW, due to differnet C++ ABI
+./waf build -o build-mingw -j2 --verbose
+cp $TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32//bin/SDL2.dll . # Install SDL2
 cp vgui_support_bin/vgui_support.dll .
 cp build-mingw/engine/xash.dll .
 cp build-mingw/mainui/menu.dll .
