752dd13c04e515ad7d25d632bc660fcf2c2f3d0e
Alibek Omarov <a1ba.omarov@gmail.com>
2019-03-29 05:44:36 +0300

    scripts: travis: uncomment osx packaging

--- scripts/build_osx_engine.sh
+++ scripts/build_osx_engine.sh
@@ -11,7 +11,7 @@ export CFLAGS="-m32"
 export CXXFLAGS="-m32"
 python waf configure --sdl2=$HOME/Library/Frameworks/SDL2.framework/ --vgui=$TRAVIS_BUILD_DIR/vgui-dev --build-type=debug || die
 python waf build -j2 || die
-# mkdir -p pkg/
-# cp engine/libxash.dylib game_launch/xash3d mainui/libxashmenu.dylib vgui_support/libvgui_support.dylib VGUI/vgui-dev-master/lib/vgui.dylib ../scripts/xash3d.sh # pkg/
-# cp ~/Library/Frameworks/SDL2.framework/SDL2 pkg/libSDL2.dylib
-# tar -cjf $TRAVIS_BUILD_DIR/xash3d-osx.tar.bz2 pkg/*
+mkdir -p pkg/
+cp build/engine/libxash.dylib build/game_launch/xash3d build/mainui/libmenu.dylib build/vgui_support/libvgui_support.dylib vgui-dev/lib/vgui.dylib pkg/
+cp ~/Library/Frameworks/SDL2.framework/SDL2 pkg/libSDL2.dylib
+tar -cjf $TRAVIS_BUILD_DIR/xash3d-osx.tar.bz2 pkg/*
