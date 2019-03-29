f9f96552b7e0c2e5884c8f0b77361f17a932d984
Alibek Omarov <a1ba.omarov@gmail.com>
2019-03-30 01:28:22 +0300

    travis: move mingw engine build to standalone VM

---
+++ scripts/travis_mingw_deps.sh
@@ -0,0 +1,3 @@
+# SDL2 for MinGW prebuilt
+curl -s http://libsdl.org/release/SDL2-devel-$SDL_VERSION-mingw.tar.gz | tar xzf -
+mv SDL2-$SDL_VERSION SDL2_mingw
--- scripts/travis_windows_deps.sh
+++ scripts/travis_windows_deps.sh
@@ -1,6 +1,3 @@
-# python 3.6
-choco install python3 -y
-
 # SDL2 for VC prebuilt
 curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip SDL2.zip
