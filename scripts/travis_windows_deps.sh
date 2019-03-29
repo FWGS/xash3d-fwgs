5436658ce7b7751bf0ba1a5ddb0c91faa5fef8ee
Alibek Omarov <a1ba.omarov@gmail.com>
2019-03-29 23:40:25 +0300

    scripts: travis: accept everything when installing python from choco

--- scripts/travis_windows_deps.sh
+++ scripts/travis_windows_deps.sh
@@ -1,9 +1,7 @@
 # python 3.6
-choco install python3
+choco install python3 -y
 
 # SDL2 for VC prebuilt
 curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
-
-cd $TRAVIS_BUILD_DIR
