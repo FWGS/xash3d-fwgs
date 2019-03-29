50c656df9400cc2cb0325cbf5c0ebfb873574690
Alibek Omarov <a1ba.omarov@gmail.com>
2019-03-29 23:48:34 +0300

    scripts: travis: fix upload to yadisk

--- scripts/yadisk_upload.sh
+++ scripts/yadisk_upload.sh
@@ -2,5 +2,6 @@ FOLDER_NAME=$DEPLOY_BRANCH-$TRAVIS_BRANCH
 WEBDAV_SRV=https://$YANDEX_DISK_USER:$YANDEX_DISK_TOKEN@webdav.yandex.ru
 
 for file in $*; do
-	curl -T $file $WEBDAV_SRV/$FOLDER_NAME
+	echo "Uploading $file..."
+	curl -T $file $WEBDAV_SRV/$FOLDER_NAME/$file
 done
