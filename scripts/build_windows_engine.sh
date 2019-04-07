146884e74772d2a505b07506b24d2c8c12fdf3a6
mittorn <mittorn@sibmail.com>
2019-04-08 02:41:27 +0700

    msvc: add pdb

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -9,12 +9,7 @@ cd $TRAVIS_BUILD_DIR
 echo After build
 
 cp $TRAVIS_BUILD_DIR/SDL2_VC/lib/x86/SDL2.dll . # Install SDL2
-cp build/vgui_support/vgui_support.dll .
 cp vgui-dev/lib/win32_vc6/vgui.dll .
-cp build/engine/xash.dll .
-cp build/mainui/menu.dll .
-cp build/ref_gl/ref_gl.dll .
-cp build/ref_soft/ref_soft.dll .
-cp build/game_launch/xash3d.exe .
+cp build/*/*.dll build/*/*.exe build/*/*.pdb .
 
 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
