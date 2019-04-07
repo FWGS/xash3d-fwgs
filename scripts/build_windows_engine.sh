3451f058dada23a182c95f42ebed8e2e95f5eb86
mittorn <mittorn@sibmail.com>
2019-04-08 05:11:37 +0700

    msvc: add pdb's from build root

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -10,6 +10,6 @@ echo After build
 
 cp $TRAVIS_BUILD_DIR/SDL2_VC/lib/x86/SDL2.dll . # Install SDL2
 cp vgui-dev/lib/win32_vc6/vgui.dll .
-cp build/*/*.dll build/*/*.exe build/*/*.pdb .
+cp build/*/*.dll build/*/*.exe build/*/*.pdb build/*.pdb .
 
 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
