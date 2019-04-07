dd18168dbc866311fc437d1d0e9d8b329c0dc816
Alibek Omarov <a1ba.omarov@gmail.com>
2019-04-08 01:41:36 +0300

    scripts: windows: use install target, include *.pdb files

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -4,12 +4,15 @@
 
 # Build engine
 cd $TRAVIS_BUILD_DIR
-./waf.bat configure --sdl2=$TRAVIS_BUILD_DIR/SDL2_VC --vgui=$TRAVIS_BUILD_DIR/vgui-dev --build-type=debug
+
+# NOTE: to build with other version use --msvc_version during configuration
+# NOTE: sometimes you may need to add WinSDK to %PATH%
+./waf.bat configure -s "$TRAVIS_BUILD_DIR/SDL2_VC" -T "debug" --prefix=`pwd`
 ./waf.bat build
 echo After build
 
 cp $TRAVIS_BUILD_DIR/SDL2_VC/lib/x86/SDL2.dll . # Install SDL2
 cp vgui-dev/lib/win32_vc6/vgui.dll .
-cp build/*/*.dll build/*/*.exe build/*/*.pdb build/*.pdb .
+./waf.bat install
 
-7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
+7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe *.pdb
