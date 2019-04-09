be0b906d24a24a44800c6a01f5edfff56c94c715
Alibek Omarov <a1ba.omarov@gmail.com>
2019-04-10 01:35:30 +0300

    scripts: travis: don't fetch ref_soft repository, as it's merged now

--- scripts/travis_common_deps.sh
+++ scripts/travis_common_deps.sh
@@ -1,5 +1,4 @@
 git clone --depth 1 https://github.com/FWGS/vgui-dev
 git clone --depth 1 https://github.com/FWGS/vgui_support_bin
 git clone --depth 1 https://github.com/FWGS/xash-extras
-git clone --depth 1 https://github.com/mittorn/ref_soft $TRAVIS_BUILD_DIR/ref_soft
 
