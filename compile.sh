0aaf624cd73a96af2c75e90cc303f7a7f6b2ed3c
Alibek Omarov <a1ba.omarov@gmail.com>
2019-05-06 04:24:42 +0300

    compile.sh: build also HLSDK. Add check if sources are symlinked, which is really handy in development

--- compile.sh
+++ compile.sh
@@ -2,8 +2,13 @@
 
 ARCHS="armeabi-v7a-hard armeabi x86"
 TOOLCHAIN=4.9
-API=21
+API=19
 ROOT="$PWD" # compile.sh must be run from root of android project sources
+SUBDIRS="xash3d-fwgs hlsdk-xash3d"
+SYMLINKS_APPEND=""
+
+# Cleanup libraries
+rm -rf android/lib/
 
 # Generate configs
 android/gen-config.sh android/
@@ -11,12 +16,22 @@ android/gen-version.sh android/
 
 build_native_project()
 {
-	./waf -t $1 -o build-$1-$2 configure -T release --android="$2,$3,$4" build
+	mkdir -p $ROOT/build-$1/$2
+	if [ -L "$1-sl" ]; then
+		cd $1-sl # need to change directory, as waf doesn't work well with symlinks(used in development purposes)
+	else
+		cd $1
+	fi
+	./waf -o $ROOT/build-$1/$2 configure -T release --android="$2,$3,$4" build || exit 1
+	./waf install --destdir=$ROOT/android/
+	cd $ROOT # obviously, we can't ../ from symlink directory, so change to our root directory
 }
 
 # Do it inside waf?
 for i in $ARCHS; do
-	build_native_project "xash3d-fwgs" "$i" "$TOOLCHAIN" "$API" || exit 1
+	for j in $SUBDIRS; do
+		build_native_project "$j" "$i" "$TOOLCHAIN" "$API"
+	done
 done
 
 # Run waf
