67ce4cfbef6e3ae4a2908822a135387b047836f7
Alibek Omarov <a1ba.omarov@gmail.com>
2019-05-23 22:13:00 +0300

    travis: windows: die if waf has failed

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -7,13 +7,13 @@ cd $TRAVIS_BUILD_DIR
 
 # NOTE: to build with other version use --msvc_version during configuration
 # NOTE: sometimes you may need to add WinSDK to %PATH%
-./waf.bat configure -s "$TRAVIS_BUILD_DIR/SDL2_VC" -T "debug" --prefix=`pwd`
-./waf.bat build -j1
+./waf.bat configure -s "$TRAVIS_BUILD_DIR/SDL2_VC" -T "debug" --prefix=`pwd` || die
+./waf.bat build -j1 || die
 echo After build
 
+./waf.bat install || die
 cp $TRAVIS_BUILD_DIR/SDL2_VC/lib/x86/SDL2.dll . # Install SDL2
 cp vgui-dev/lib/win32_vc6/vgui.dll .
-./waf.bat install
 
 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe *.pdb
 
