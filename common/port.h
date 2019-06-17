e9f26fe29d90d79aa4679b5c9fd8a1a65ec80851
Alibek Omarov <a1ba.omarov@gmail.com>
2019-06-17 08:37:51 +0300

    port: don't define XASH_64BIT, as it defined by waf

--- common/port.h
+++ common/port.h
@@ -17,9 +17,10 @@ GNU General Public License for more details.
 #ifndef PORT_H
 #define PORT_H
 
-#if defined(__LP64__) || defined(__LLP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
-  #define XASH_64BIT
-#endif
+// a1ba: disabled, set by waf
+// #if defined(__LP64__) || defined(__LLP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
+//   #define XASH_64BIT
+// #endif
 
 // #ifdef XASH_64BIT
 // #define ARCH_SUFFIX "64"
