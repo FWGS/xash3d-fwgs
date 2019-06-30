03660352378492e125e108f16d69c23d801cb719
Alibek Omarov <a1ba.omarov@gmail.com>
2019-07-01 00:40:40 +0300

    travis: add github releases uploader (#47)

---
+++ scripts/continious_upload.sh
@@ -0,0 +1,27 @@
+##################################
+#
+#        GitHub Releases
+#
+##################################
+
+set +x
+
+wget -O upload.sh "https://raw.githubusercontent.com/FWGS/uploadtool/master/upload.sh"
+chmod +x upload.sh
+
+export GITHUB_TOKEN=$GH_TOKEN
+./upload.sh $*
+
+##################################
+#
+#        Yandex.Disk
+#
+##################################
+
+FOLDER_NAME=$DEPLOY_BRANCH-$TRAVIS_BRANCH
+WEBDAV_SRV=https://$YANDEX_DISK_USER:$YANDEX_DISK_TOKEN@webdav.yandex.ru
+
+for file in $*; do
+	echo "Uploading $file..."
+	curl -T $file $WEBDAV_SRV/$FOLDER_NAME/$file
+done
