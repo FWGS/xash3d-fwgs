61f49938e5719b50e2db5fc2963d54da1c7a4cee
Alibek Omarov <a1ba.omarov@gmail.com>
2019-07-29 08:10:04 +0300

    travis: build for android, not publish until touch controls are not done

---
+++ scripts/travis_android_deps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Download HLSDK"
+
+cd $TRAVIS_BUILD_DIR
+git clone --depth 1 --recursive https://github.com/FWGS/hlsdk-xash3d hlsdk || exit 1
+
+echo "Download Android SDK"
+mkdir -p sdk && cd sdk
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -qO sdk.zip > /dev/null 2>/dev/null || exit 1
+
+echo "Unpack Android SDK"
+unzip sdk.zip > /dev/null 2>/dev/null || exit 1
+
+echo "Download all needed tools and NDK"
+cd tools/bin || exit 1
+./sdkmanager --install build-tools\;29.0.1 platform-tools platforms\;android-19 ndk-bundle
+
+echo "Download Xash3D FWGS Android source"
+git clone --depth 1 https://github.com/FWGS/xash3d-android-project -b waf android || exit 1
+cd android
+
+echo "Fetching submodules"
+git submodule update --init xash-extras || exit 1
+
+ln -s $TRAVIS_BUILD_DIR xash3d-fwgs-sl
+echo "Installed Xash3D FWGS source symlink"
+
+ln -s $TRAVIS_BUILD_DIR/hlsdk hlsdk-xash3d-sl
+echo "Install HLSDK source symlink"
+
+cd $TRAVIS_BUILD_DIR
