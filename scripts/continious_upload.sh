bc39b707d484f1596b57b63c9c867af8533ce89f
Alibek Omarov <a1ba.omarov@gmail.com>
2019-09-29 18:38:19 +0300

    Revert github repository deploy removal

--- scripts/continious_upload.sh
+++ scripts/continious_upload.sh
@@ -11,3 +11,17 @@ chmod +x upload.sh
 
 export GITHUB_TOKEN=$GH_TOKEN
 ./upload.sh $*
+
+##################################
+#
+#        Yandex.Disk
+#
+##################################
+
+FOLDER_NAME=$DEPLOY_BRANCH-$TRAVIS_BRANCH
+WEBDAV_SRV=https://$YANDEX_DISK_USER:$YANDEX_DISK_TOKEN@webdav.yandex.ru
+
+for file in $*; do
+	echo "Uploading $file..."
+	curl -T $file $WEBDAV_SRV/$FOLDER_NAME/$file
+done
