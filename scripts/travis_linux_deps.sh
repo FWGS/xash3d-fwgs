a38f561d7ceb71ff95b804f7ea9aabed798c56af
Alibek Omarov <a1ba.omarov@gmail.com>
2019-10-16 02:18:12 +0300

    ci: use SDL2 zip source archive

--- scripts/travis_linux_deps.sh
+++ scripts/travis_linux_deps.sh
@@ -1,3 +1,4 @@
 # SDL2 sources. We will build our own version
-curl -s http://libsdl.org/release/SDL2-$SDL_VERSION.tar.gz | tar xzf -
+curl http://libsdl.org/release/SDL2-$SDL_VERSION.zip -o SDL2.zip
+unzip -x SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_src
