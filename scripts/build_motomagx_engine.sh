f09682131f4335518e976a48c69f14998ace022c
Alibek Omarov <a1ba.omarov@gmail.com>
2019-11-07 08:41:10 +0300

    travis: fix magx deploy

--- scripts/build_motomagx_engine.sh
+++ scripts/build_motomagx_engine.sh
@@ -10,8 +10,8 @@ mkdir -p Xash/valve/dlls
 
 cd hlsdk
 ./waf configure -T fast --enable-magx build
-cp build/cl_dlls/client.so ../Xash/valve/cl_dlls/client_armv6l.so
-cp build/dlls/hl.so ../Xash/valve/cl_dlls/hl_armv6l.so
+cp build/cl_dll/client.so ../Xash/valve/cl_dlls/client_armv6l.so
+cp build/dlls/hl.so ../Xash/valve/dlls/hl_armv6l.so
 cd ../
 
 ./waf configure -T fast --enable-magx --win-style-install --prefix='' build install --destdir=Xash/
