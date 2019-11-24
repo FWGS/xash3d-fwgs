755db4d56b84f413697f52567ebaa8c3d483c0a0
Alibek Omarov <a1ba.omarov@gmail.com>
2019-11-24 03:00:35 +0300

    backends: reformat

--- common/backends.h
+++ common/backends.h
@@ -1,5 +1,5 @@
 /*
-backends.h - backend macro definations
+backends.h - backend macro definitions
 Copyright (C) 2016 Mittorn
 
 This program is free software: you can redistribute it and/or modify
@@ -12,45 +12,43 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
-
-
 #ifndef BACKENDS_H
 #define BACKENDS_H
 
 // video backends (XASH_VIDEO)
-#define VIDEO_NULL 0
-#define VIDEO_SDL 1
+#define VIDEO_NULL    0
+#define VIDEO_SDL     1
 #define VIDEO_ANDROID 2
-#define VIDEO_FBDEV 3
+#define VIDEO_FBDEV   3
 
 // audio backends (XASH_SOUND)
-#define SOUND_NULL 0
-#define SOUND_SDL 1
+#define SOUND_NULL     0
+#define SOUND_SDL      1
 #define SOUND_OPENSLES 2
-#define SOUND_ALSA 3
+#define SOUND_ALSA     3
 
 // crash handler (XASH_CRASHHANDLER)
-#define CRASHHANDLER_NULL 0
+#define CRASHHANDLER_NULL     0
 #define CRASHHANDLER_UCONTEXT 1
-#define CRASHHANDLER_DBGHELP 2
-#define CRASHHANDLER_WIN32 3
+#define CRASHHANDLER_DBGHELP  2
+#define CRASHHANDLER_WIN32    3
 
 // input (XASH_INPUT)
-#define INPUT_NULL 0
-#define INPUT_SDL 1
+#define INPUT_NULL    0
+#define INPUT_SDL     1
 #define INPUT_ANDROID 2
-#define INPUT_EVDEV 3
+#define INPUT_EVDEV   3
 
 // timer (XASH_TIMER)
-#define TIMER_NULL 0 // not used
-#define TIMER_SDL 1
+#define TIMER_NULL  0 // not used
+#define TIMER_SDL   1
 #define TIMER_LINUX 2
 #define TIMER_WIN32 3
 
 // messageboxes (XASH_MESSAGEBOX)
-#define MSGBOX_STDERR 0
-#define MSGBOX_SDL 1
+#define MSGBOX_STDERR  0
+#define MSGBOX_SDL     1
 #define MSGBOX_ANDROID 2
-#define MSGBOX_WIN32 3
+#define MSGBOX_WIN32   3
 
 #endif /* BACKENDS_H */
