070272489d30e83b0ae3d8839fb73489d131dde4
mittorn <mittorn@sibmail.com>
2020-02-08 22:41:47 +0700

    library: fix saverestore offsets code from old engine

--- engine/common/lib_common.c
+++ engine/common/lib_common.c
@@ -39,8 +39,8 @@ void COM_PushLibraryError( const char *error )
 void *COM_FunctionFromName_SR( void *hInstance, const char *pName )
 {
 #ifdef XASH_ALLOW_SAVERESTORE_OFFSETS
-	if( !Q_memcmp( pName, "ofs:",4 ) )
-		return svgame.dllFuncs.pfnGameInit + Q_atoi(pName + 4);
+	if( !memcmp( pName, "ofs:",4 ) )
+		return (byte*)svgame.dllFuncs.pfnGameInit + Q_atoi(pName + 4);
 #endif
 	return COM_FunctionFromName( hInstance, pName );
 }
