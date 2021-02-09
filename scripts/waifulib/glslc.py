074e4fd540a32e41b7047a8ca6eaf56076c82698
Alibek Omarov <a1ba.omarov@gmail.com>
2021-02-10 02:48:30 +0300

    glslc: rework tool, support dependency scan, preliminary work on making tool dependless from compiler(like ccroot)

--- scripts/waifulib/glslc.py
+++ scripts/waifulib/glslc.py
@@ -3,29 +3,42 @@
 
 import os
 from waflib import *
-from waflib.Tools import c_preproc
-
-def options(opt):
-	pass
+from waflib.Tools import c_preproc, ccroot
 
 def configure(conf):
 	conf.find_program('glslc')
 
-	self.env.GLSLCFLAGS    = []
+	conf.add_os_flags('GLSLCPPFLAGS', dup=False)
+	conf.add_os_flags('GLSLCFLAGS', dup=False)
+
+	v = conf.env
+
+	v.GLSLCINCLUDES = []
+	v.GLSLCDEFINES  = []
+
+	v.GLSLCPPPATH_ST = '-I%s'
+	v.GLSLDEFINES_ST = '-D%s'
+	v.GLSLC_SRC_F    = []
+	v.GLSLC_TGT_F    = ['-c', '-o']
 
 class glsl(Task.Task):
-	color = 'BLUE'
-	run_str = '${GLSLC} ${GLSLCFLAGS} ${GLSLCINCLUDES} ${GLSLCDEFINES} -o ${TGT} ${SRC}'
-	vars = ['GLSLC', 'GLSLCFLAGS', 'GLSLCINCLUDES', 'GLSLCDEFINES']
+	color = 'PINK'
+	run_str = '${GLSLC} ${GLSLCFLAGS} ${GLSLCPPPATH_ST:INCPATHS} ${GLSLDEFINES_ST:GLSLCDEFINES} ${GLSLC_SRC_F}${SRC} ${GLSLC_TGT_F}${TGT[0].abspath()} ${GLSLCPPFLAGS}'
+	vars = ['GLSLCDEPS'] # unused variable to depend on, just in case
+	ext_in  = ['.h'] # set the build order easily by using ext_out=['.h']
+	scan = c_preproc.scan
+
+	def keyword(self):
+		return 'Compiling shader'
 
 @TaskGen.extension('.vert', '.frag')
 def process_glsl_source(self, src):
-	includes = self.to_list(getattr(self, 'includes', []))
-	defines  = self.to_list(getattr(self, 'defines', []))
-	sources  = self.to_list(getattr(self, 'source', []))
-
-	self.env.GLSLCINCLUDES = ['-I' + s for s in includes]
-	self.env.GLSLCDEFINES  = ['-D' + s for s in defines]
+	# see ccroot.apply_incpaths
+	lst = self.to_incnodes(self.to_list(getattr(self, 'includes', [])) + self.env.GLSLCINCLUDES)
+	self.includes_nodes = lst
+	cwd = self.get_cwd()
+	self.env.INCPATHS = [x.path_from(cwd) for x in lst]
+	self.env.append_unique('GLSLCDEFINES', self.to_list(getattr(self, 'defines', [])))
 
 	tsk = self.create_task('glsl', src, src.parent.find_or_declare('%s.spv' % src.name))
 
