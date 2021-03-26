9ea45a71f57ad0ccab0992d62ff5e62f2d7ffa6a
Alibek Omarov #SupportRMS <a1ba.omarov@gmail.com>
2021-03-26 17:57:51 +0300

    scripts: rename win32 build to xash3d-win32-vc.7z

--- scripts/build_windows_engine.sh
+++ scripts/build_windows_engine.sh
@@ -15,7 +15,7 @@ echo After build
 cp $TRAVIS_BUILD_DIR/SDL2_VC/lib/x86/SDL2.dll . # Install SDL2
 cp vgui-dev/lib/win32_vc6/vgui.dll .
 
-7z a -t7z $TRAVIS_BUILD_DIR/xash3d-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe *.pdb
+7z a -t7z $TRAVIS_BUILD_DIR/xash3d-win32-vc.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe *.pdb
 
 echo "Generating VC2008 project"
 rm -rf vc2008/
