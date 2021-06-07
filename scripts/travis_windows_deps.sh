931de39f239be4d3c0781700297d9fec4ad72baa
Ivan Avdeev <me@w23.ru>
2021-06-06 23:56:24 -0700

    ref_vk: try to add vulkan sdk installation on windows

--- scripts/travis_windows_deps.sh
+++ scripts/travis_windows_deps.sh
@@ -2,3 +2,6 @@
 curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
+
+curl -L --show-error --output VulkanSDK.exe https://vulkan.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/windows/VulkanSDK-$VULKAN_SDK_VERSION-Installer.exe?Human=true
+runas /user:administrator VulkanSDK.exe /S
