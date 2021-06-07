1b3b4e51cde2ec81303437cbec030104061ce27a
Ivan Avdeev <me@w23.ru>
2021-06-07 10:42:41 -0700

    ... why

--- scripts/travis_windows_deps.sh
+++ scripts/travis_windows_deps.sh
@@ -3,9 +3,15 @@ curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
 
+set -eux
+
+date
 curl -L --show-error --output vulkan_sdk.exe https://vulkan.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/windows/vulkan_sdk.exe
+date
 echo "Installing Vulkan SDK $VULKAN_SDK_VERSION"
+date
 runas /user:administrator vulkan_sdk.exe /S
+date
 echo "Installed"
 
 export VULKAN_SDK=C:/VulkanSDK/$VULKAN_SDK_VERSION
