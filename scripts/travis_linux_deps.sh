844eedba93454082ab55f245d2bf9908a9bff1c9
Ivan Avdeev <me@w23.ru>
2021-06-06 21:00:24 -0700

    attempt to add vulkan linux deps for travis

--- scripts/travis_linux_deps.sh
+++ scripts/travis_linux_deps.sh
@@ -2,3 +2,9 @@
 curl http://libsdl.org/release/SDL2-$SDL_VERSION.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_src
+
+# ref_vk required Vulkan SDK
+wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-focal.list https://packages.lunarg.com/vulkan/lunarg-vulkan-focal.list
+sudo apt update
+sudo apt install vulkan-sdk
