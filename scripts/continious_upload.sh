5723f741dfe648302a67756e2299bc71fa6ad369
Alibek Omarov <a1ba.omarov@gmail.com>
2021-06-29 01:43:15 +0300

    github: first try to upload to GitHub Releases, enable amd64 win32 builds

--- scripts/continious_upload.sh
+++ scripts/continious_upload.sh
@@ -6,11 +6,8 @@
 
 set +x
 
-# Disabled until GitHub sends prereleases to email
-
 wget -O upload.sh "https://raw.githubusercontent.com/probonopd/uploadtool/master/upload.sh"
 chmod +x upload.sh
 
-export GITHUB_TOKEN=$GH_TOKEN
 ./upload.sh $*
 
