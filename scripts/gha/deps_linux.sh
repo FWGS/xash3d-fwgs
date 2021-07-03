affc64c6da41e81546b05892757b2c5749393d6f
Alibek Omarov <a1ba.omarov@gmail.com>
2021-07-03 23:18:14 +0300

    gha: don't install unneeded multilib packages for amd64

--- scripts/gha/deps_linux.sh
+++ scripts/gha/deps_linux.sh
@@ -10,7 +10,7 @@ if [ "$GH_CPU_ARCH" == "i386" ]; then
 	wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-i686.AppImage" -O appimagetool.AppImage
 elif [ "$GH_CPU_ARCH" == "amd64" ]; then
 	sudo apt update
-	sudo apt install gcc-multilib g++-multilib libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g
+	sudo apt install libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g
 
 	wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool.AppImage
 else
