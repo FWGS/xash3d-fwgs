a4ec54551afd7a51606c6980acd77dee2528fb83
Alibek Omarov <a1ba.omarov@gmail.com>
2021-07-03 22:45:14 +0300

    android: fix ampersand, lt, gt in version generator script

--- android/gen-version.sh
+++ android/gen-version.sh
@@ -8,8 +8,11 @@ git update-index --assume-unchanged $GIT_REV_XML
 echo '<?xml version="1.0" encoding="utf-8"?>' > $GIT_REV_XML
 echo '<resources>' >> $GIT_REV_XML
 echo -n '<string name="git_revisions">' >> $GIT_REV_XML
-echo -n '<b>Version information:</b> ' $*_endl| sed -e s/_endl/'\\n'/ >> $GIT_REV_XML
-git submodule --quiet foreach --recursive 'echo -n \<b\>`basename $name`:\</b\>\ `git log --abbrev-commit --pretty=oneline -1` _endl' | sed -e "s|_endl|\\\n|g; s|'|\\\'|g" >> $GIT_REV_XML
+echo -n '<b>Version information:</b>\n' >> $GIT_REV_XML
+git submodule --quiet foreach --recursive \
+	'echo -n \<b\>`basename $name`:\</b\>\ \
+`git log --abbrev-commit --pretty=oneline -1 | sed "s/\&/\&amp/g;s/>/&gt/g;s/</&lt/g"`\\\\n' \
+| sed -e "s|'|\\\'|g" >> $GIT_REV_XML
 echo -n $USER@$(hostname) $(date +%H:%M:%S-%d-%m-%y) >> $GIT_REV_XML
 echo '</string>' >> $GIT_REV_XML
 echo '</resources>' >> $GIT_REV_XML
