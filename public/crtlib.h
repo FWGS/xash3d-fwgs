c64a9fffa46bf19e7452164a8d1a204541c0820e
Alibek Omarov <a1ba.omarov@gmail.com>
2021-10-01 20:38:52 +0300

    public: ParseFile drops const qualifier, return -1 length on overflow
    As we usually put allocated data to ParseFile, we don't need const qualifier

--- public/crtlib.h
+++ public/crtlib.h
@@ -95,7 +95,7 @@ char COM_Hex2Char( uint8_t hex );
 void COM_Hex2String( uint8_t hex, char *str );
 #define COM_CheckString( string ) ( ( !string || !*string ) ? 0 : 1 )
 #define COM_CheckStringEmpty( string ) ( ( !*string ) ? 0 : 1 )
-const char *_COM_ParseFileSafe( const char *data, char *token, const int size, unsigned int flags, int *len );
+char *_COM_ParseFileSafe( char *data, char *token, const int size, unsigned int flags, int *len );
 #define COM_ParseFile( data, token, size ) _COM_ParseFileSafe( data, token, size, 0, NULL )
 #define COM_ParseFileLegacy( data, token ) COM_ParseFileSafe( data, token, INT_MAX )
 int matchpattern( const char *in, const char *pattern, qboolean caseinsensitive );
