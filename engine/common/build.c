b23d5ed3547883971d1f3bd7b1cf3e3ceef3251e
Jesse Buhagiar <jooster669@gmail.com>
2022-01-02 00:39:02 +1100

    Build: Add SerenityOS to list of compatible systems
    This is required by the build system to spit out a library with
    the correct name/platform.

--- engine/common/build.c
+++ engine/common/build.c
@@ -95,6 +95,8 @@ const char *Q_buildos( void )
 	osname = "DOS4GW";
 #elif XASH_HAIKU
 	osname = "haiku";
+#elif XASH_SERENITY
+	osname = "serenityos";
 #else
 #error "Place your operating system name here! If this is a mistake, try to fix conditions above and report a bug"
 #endif
