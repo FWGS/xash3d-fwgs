7821f425e7a49ce3ca81fc9c17c59c5a1116bc01
Alibek Omarov <a1ba.omarov@gmail.com>
2022-02-24 05:45:41 +0300

    engine: client: position history indexes can't be negative, use unsigned with them

--- engine/client/cl_frame.c
+++ engine/client/cl_frame.c
@@ -349,7 +349,7 @@ find two timestamps
 qboolean CL_FindInterpolationUpdates( cl_entity_t *ent, float targettime, position_history_t **ph0, position_history_t **ph1 )
 {
 	qboolean	extrapolate = true;
-	int	i, i0, i1, imod;
+	uint		i, i0, i1, imod;
 	float	at;
 
 	imod = ent->current_position;
