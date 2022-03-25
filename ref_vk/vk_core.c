24bdf313cbf12cbc72d79ea2b64c53dc105bdc7a
tinyend <rowbert121@outlook.com>
2022-03-25 16:38:30 +0800

    init device

--- ref_vk/vk_core.c
+++ ref_vk/vk_core.c
@@ -1,4 +1,5 @@
 
+#include "physint.h"
 #include "vk_common.h"
 #include "vk_textures.h"
 
@@ -11,23 +12,33 @@
 #include "eiface.h"
 #include <stdint.h>
 
-
 #define VK_NO_PROTOTYPES
 #include <vulkan/vulkan.h>
 
+typedef struct physical_device_s {
+    VkPhysicalDevice device;
+    VkPhysicalDeviceMemoryProperties memory_properties;
+}physics_device_t;
+
 typedef struct vulkan_core_s {
-    PFN_vkGetInstanceProcAddr get_proc_addr;
     uint32_t vulkan_version;
     VkInstance instance;
     VkDebugUtilsMessengerEXT  debug_messenger;
     poolhandle_t pool;
     qboolean debug;
+
+    VkSurfaceKHR surface;
+    physics_device_t physical_devices;
+    VkDevice device;
+    VkQueue graphics_queue;
 }vulkan_core_t;
 
 vulkan_core_t vk_core;
 
+static PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr;
+
 const char* validation_layers[] = {
-    "VK_LAYER_LUNARG_standard_validaction",
+    "VK_LAYER_LUNARG_standard_validation",
 };
 
 static const char* resultName(VkResult result) {
@@ -46,7 +57,7 @@ static const char* resultName(VkResult result) {
 
 
 #define XVK_INSTANCE_FUNC(f) \
-    (PFN_##f)vk_core.get_proc_addr(vk_core.instance, #f)
+    (PFN_##f)vkGetInstanceProcAddr(vk_core.instance, #f)
 
 
 #define DEFINE_FUNC_VAR(f) PFN_##f f
@@ -65,17 +76,22 @@ static const char* resultName(VkResult result) {
     X(vkGetPhysicalDeviceSurfaceSupportKHR)             \
     X(vkGetPhysicalDeviceQueueFamilyProperties)         \
     X(vkGetPhysicalDeviceMemoryProperties)              \
-    X(vkCreateDevice)
+    X(vkCreateDevice)                                   \
+    X(vkGetDeviceProcAddr)  
 
 #define INST_DEBUG_FUNC(X) \
     X(vkCreateDebugUtilsMessengerEXT)                   \
     X(vkDestroyDebugUtilsMessengerEXT)                  
+
+#define DEVICE_FUNCS(X)  \   
+    X(vkGetDeviceQueue)     
 
 
 #define X(f) PFN_##f f = NULL;  
     NULLINST_FUNC(X)
     INST_FUNC(X)
     INST_DEBUG_FUNC(X)
+    DEVICE_FUNCS(X)
 #undef X
 
 static dllfunc_t nulllist_funcs[] = {
@@ -96,6 +112,12 @@ static dllfunc_t debug_funcs[] = {
 #undef X
 };
 
+static dllfunc_t device_funcs[] = {
+#define X(f) {#f, (void**)&f},
+    DEVICE_FUNCS(X)
+#undef X
+};
+
 #define XVK_CHECK(f)                                                            \
     do {                                                                        \
         const VkResult _f = (f);                                                \
@@ -105,16 +127,29 @@ static dllfunc_t debug_funcs[] = {
         }                                                                       \
     }while(0)
 
+
 static void loadInstanceFunction(dllfunc_t *funcs, int count) 
 {
     for (int i= 0; i < count; i++) {
-        *funcs[i].func = vk_core.get_proc_addr(vk_core.instance,funcs[i].name);  
+        *funcs[i].func = vkGetDeviceProcAddr(vk_core.device, funcs[i].name);  
         if (!*funcs[i].func) {
             gEngine.Con_Printf(S_WARN "Function: %s was not load\n",
             funcs[i].name);
         }
     }
 }
+
+static void loadDeviceFunction(dllfunc_t *funcs, int count) 
+{
+    for (int i= 0; i < count; i++) {
+        *funcs[i].func = vkGetInstanceProcAddr(vk_core.instance,funcs[i].name);  
+        if (!*funcs[i].func) {
+            gEngine.Con_Printf(S_WARN "Function: %s was not load\n",
+            funcs[i].name);
+        }
+    }
+}
+
 
 static VkBool32 debugCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -141,9 +176,9 @@ static qboolean createInstance(void)
     app_info.apiVersion = VK_VERSION_1_0;
     app_info.engineVersion = VK_VERSION_1_0;
     app_info.applicationVersion = VK_VERSION_1_0;
+    app_info.pNext = NULL;
 
-    
-    // instanceCI.pApplicationInfo = &app_info;
+ //   instanceCI.pApplicationInfo = &app_info;
 
     vid_extension = gEngine.VK_GetInstanceExtension(0, instance_extension);
     if (vid_extension < 0)
@@ -151,7 +186,6 @@ static qboolean createInstance(void)
         gEngine.Con_Printf(S_ERROR "Init Vulkan Extension Failed\n");
         return false;
     }
-    gEngine.Con_Printf(S_WARN "Vid Extension Count: %d\n", vid_extension);
     num_instance_extension += vid_extension;
 
     instance_extension = Mem_Malloc(vk_core.pool, sizeof(const char*) * num_instance_extension);
@@ -195,12 +229,74 @@ static qboolean createInstance(void)
             gEngine.Con_Printf(S_WARN "validation unavailable\n");
         }
     }
+
     Mem_Free(instance_extension);
     return true;
 }
 
-static qboolean createDevice()
+static qboolean createDevice(void)
 {
+    VkPhysicalDevice* physical_devices = NULL;
+    uint32_t num_physical_device = 0u;
+    uint32_t best_device_index =UINT32_MAX;
+    uint32_t queue_index = UINT32_MAX;
+    float prio =1.f;
+    XVK_CHECK(vkEnumeratePhysicalDevices(vk_core.instance, &num_physical_device, NULL));
+
+    physical_devices = Mem_Malloc(vk_core.pool, sizeof(VkPhysicalDevice)* num_physical_device);
+    XVK_CHECK(vkEnumeratePhysicalDevices(vk_core.instance, &num_physical_device, physical_devices));
+
+    for (uint32_t i = 0; i < num_physical_device; ++i) {
+        VkQueueFamilyProperties* queue_family_properties =NULL;
+        uint32_t num_queue_family_prperties =0;
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_devices[i], &num_queue_family_prperties, NULL);
+        queue_family_properties = Mem_Malloc(vk_core.pool, sizeof(VkQueueFamilyProperties)* num_queue_family_prperties);
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_devices[i], &num_queue_family_prperties, queue_family_properties);
+        for (uint32_t j = 0; j < num_queue_family_prperties; ++j) {
+            VkBool32 present =false;
+            if (!(queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
+                continue;
+            }
+            vkGetPhysicalDeviceSurfaceSupportKHR(physical_devices[i], j, vk_core.surface, &present);
+            if (!present) {
+                continue;
+            }
+            queue_index = i;
+            break;
+        }
+        /// todo select more device
+        if (queue_index < num_queue_family_prperties ) {
+            best_device_index = i;
+            break;
+        }
+        Mem_Free(queue_family_properties);
+    }
+
+    if (best_device_index < num_physical_device) {
+          const char* device_extension[] ={
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        };
+        VkDeviceQueueCreateInfo queue_info ={VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO};
+        VkDeviceCreateInfo create_info = {VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO};
+        queue_info.queueFamilyIndex = queue_index;
+        queue_info.queueCount = 1;
+        queue_info.pQueuePriorities = &prio;
+    
+        create_info.queueCreateInfoCount =1;
+        create_info.pQueueCreateInfos = &queue_info;
+        create_info.enabledExtensionCount = ARRAYSIZE(device_extension);
+        create_info.ppEnabledExtensionNames= device_extension;
+
+        vk_core.physical_devices.device = physical_devices[best_device_index];
+        vkGetPhysicalDeviceMemoryProperties(vk_core.physical_devices.device, &vk_core.physical_devices.memory_properties);
+        XVK_CHECK(vkCreateDevice( vk_core.physical_devices.device, &create_info, NULL, &vk_core.device));
+        loadDeviceFunction(device_funcs, ARRAYSIZE(device_funcs));
+        vkGetDeviceQueue(vk_core.device, 0, 0, &vk_core.graphics_queue);
+    }
+
+
+    Mem_Free(physical_devices);
+
     return true;
 }
 // construct, destruct
@@ -213,8 +309,8 @@ qboolean R_VkInit( void )
         return false;
     }
 
-    vk_core.get_proc_addr = gEngine.VK_GetVkGetInstanceProcAddr();
-    if (!vk_core.get_proc_addr) {
+    vkGetInstanceProcAddr = gEngine.VK_GetVkGetInstanceProcAddr();
+    if (!vkGetInstanceProcAddr) {
         gEngine.Con_Printf(S_ERROR "Init Vulkan Extension Failed\n");
         return false;
     }
@@ -231,10 +327,15 @@ qboolean R_VkInit( void )
     if(!createInstance()){
         return false;
     }
+    vk_core.surface = gEngine.VK_CreateSurface(vk_core.instance);
+    if (!vk_core.surface) {
+        gEngine.Con_Printf(S_ERROR "Create Surface Failed\n");
+        return false;
+    }
     if (!createDevice()) {
         return false;
     }
-	
+
 	initTextures();
     
     return true;
@@ -249,5 +350,7 @@ void R_VkShutdown( void )
     }
     vkDestroyInstance(vk_core.instance, NULL);
     vk_core.instance = VK_NULL_HANDLE;
+
+    Mem_FreePool(&vk_core.pool);
 
 }
