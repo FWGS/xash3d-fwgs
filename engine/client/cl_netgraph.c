d4bb5423aeb6886479af9619c3a12b0921ad324f
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-06-16 00:41:11 +0400

    engine: client: cl_netgraph: set rendermode to transparent instead additive

--- engine/client/cl_netgraph.c
+++ engine/client/cl_netgraph.c
@@ -673,7 +673,7 @@ void SCR_DrawNetGraph( void )
 
 	if( graphtype < 3 )
 	{
-		ref.dllFuncs.GL_SetRenderMode( kRenderTransAdd );
+		ref.dllFuncs.GL_SetRenderMode( kRenderTransColor );
 		ref.dllFuncs.GL_Bind( XASH_TEXTURE0, R_GetBuiltinTexture( REF_WHITE_TEXTURE ) );
 		ref.dllFuncs.Begin( TRI_QUADS ); // draw all the fills as a long solid sequence of quads for speedup reasons
 
