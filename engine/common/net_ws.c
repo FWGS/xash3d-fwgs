ac05acf6dcdc79524fdcbf50e7187713acdabdbe
Alibek Omarov <a1ba.omarov@gmail.com>
2022-08-23 20:04:59 +0300

    engine: network: I'm fucking blind

--- engine/common/net_ws.c
+++ engine/common/net_ws.c
@@ -616,7 +616,7 @@ qboolean NET_IsReservedAdr( netadr_t a )
 
 	if( a.type == NA_IP )
 	{
-		if( a.ip[0] == 10 )
+		if( a.ip[0] == 10 || a.ip[0] == 127 )
 			return true;
 
 		if( a.ip[0] == 172 && a.ip[1] >= 16 )
