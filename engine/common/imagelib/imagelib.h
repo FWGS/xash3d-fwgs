ccf7619ae51fb9072b861df50a802901eead9492
Alibek Omarov <a1ba.omarov@gmail.com>
2022-12-05 03:18:27 +0300

    engine: imagelib: refactor image loading function

--- engine/common/imagelib/imagelib.h
+++ engine/common/imagelib/imagelib.h
@@ -91,7 +91,7 @@ typedef struct imglib_s
 	// global parms
 	rgba_t			fogParams;	// some water textures has info about underwater fog
 
-	image_hint_t		hint;		// hint for some loaders
+	int			hint;		// hint for some loaders
 	byte			*tempbuffer;	// for convert operations
 	int			cmd_flags;	// global imglib flags
 	int			force_flags;	// override cmd_flags
@@ -171,7 +171,6 @@ rgbdata_t *Image_Quantize( rgbdata_t *pic );
 // img_utils.c
 //
 void Image_Reset( void );
-rgbdata_t *ImagePack( void );
 byte *Image_Copy( size_t size );
 void Image_CopyParms( rgbdata_t *src );
 qboolean Image_ValidSize( const char *name );
