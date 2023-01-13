0ff2161c6a32595da0bdedcd62d4f2c470ac1464
Xav101 <xaviernye@msn.com>
2023-01-13 02:28:20 -0600

    Added GPL copyright notice and description

--- engine/platform/irix/dladdr.c
+++ engine/platform/irix/dladdr.c
@@ -1,4 +1,17 @@
 /*
+dladdr.c - dladdr implementation for SGI IRIX
+Copyright (C) 2022 Xav101
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+/*
  * From SGI IRIX's 'man dladdr'
  *
  * <dlfcn.h> does not contain a prototype for dladdr or definition of
--- engine/platform/irix/dladdr.h
+++ engine/platform/irix/dladdr.h
@@ -1,3 +1,18 @@
+/*
+dladdr.h - dladdr prototypes for SGI IRIX
+Copyright (C) 2022 Xav101
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+/* See engine/platform/irix/dladdr.c for the requirement for this implementation */
+
 #ifndef DLADDR_IRIX_H
 #define DLADDR_IRIX_H
 
