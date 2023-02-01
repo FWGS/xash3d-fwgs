f3ff942ea900b1b51b2e3d7f8a82996291a0e7c4
Alibek Omarov <a1ba.omarov@gmail.com>
2023-02-01 04:58:12 +0300

    scripts: flatpak: add basic i386 flatpak script

---
+++ scripts/flatpak/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Xash3D FWGS installed as Flatpak."
+
+# TODO: detect by libraryfolders.vdf and installed apps
+HALFLIFESTEAMDIR="$HOME/.steam/steam/steamapps/common/Half-Life"
+
+if [ -d "$HALFLIFESTEAMDIR" ]; then
+	echo "Detected Half-Life installation in $HALFLIFESTEAMDIR, using as RoDir"
+	export XASH3D_RODIR=$HALFLIFESTEAMDIR
+fi
+
+XASHDATADIR="$HOME/.xash/"
+
+mkdir -p $XASHDATADIR
+export XASH3D_BASEDIR="$XASHDATADIR"
+echo "Base directory is $XASH3D_BASEDIR"
+
+export XASH3D_EXTRAS_PAK1=/app/share/xash3d/valve/extras.pk3
+exec /app/lib32/xash3d/xash3d "$@"
