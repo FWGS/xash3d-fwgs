30b698067a00c57d3c50d6fd39e967c82e853d3d
Alibek Omarov <a1ba.omarov@gmail.com>
2023-02-05 03:25:07 +0300

    scripts: flatpak: allow easy debugger attach

--- scripts/flatpak/run.sh
+++ scripts/flatpak/run.sh
@@ -17,4 +17,4 @@ export XASH3D_BASEDIR="$XASHDATADIR"
 echo "Base directory is $XASH3D_BASEDIR"
 
 export XASH3D_EXTRAS_PAK1=/app/share/xash3d/valve/extras.pk3
-exec /app/lib32/xash3d/xash3d "$@"
+exec $DEBUGGER /app/lib32/xash3d/xash3d "$@"
