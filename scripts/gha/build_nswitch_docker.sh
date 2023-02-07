749ac5ed544bf325820fb6ab6c1f24133252cb01
fgsfds <derp.primus@gmail.com>
2023-02-07 20:38:40 +0100

    ci: nswitch: do not rely on dkp-pacman at all

--- scripts/gha/build_nswitch_docker.sh
+++ scripts/gha/build_nswitch_docker.sh
@@ -14,13 +14,27 @@ build_hlsdk()
   ./waf clean
 }
 
-echo "Downloading missing deps..."
+echo "Setting up environment..."
 
-dkp-pacman -S --noconfirm dkp-toolchain-vars || die
+# we can't actually download dkp-toolchain-vars even from here, so
+export PORTLIBS_ROOT=${DEVKITPRO}/portlibs
+export PATH=${DEVKITPRO}/tools/bin:${DEVKITPRO}/devkitA64/bin:$PATH
+export TOOL_PREFIX=aarch64-none-elf-
+export CC=${TOOL_PREFIX}gcc
+export CXX=${TOOL_PREFIX}g++
+export AR=${TOOL_PREFIX}gcc-ar
+export RANLIB=${TOOL_PREFIX}gcc-ranlib
+export PORTLIBS_PREFIX=${DEVKITPRO}/portlibs/switch
+export PATH=$PORTLIBS_PREFIX/bin:$PATH
+export ARCH="-march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec"
+export CFLAGS="${ARCH} -O2 -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-D__SWITCH__ -I${PORTLIBS_PREFIX}/include -isystem ${DEVKITPRO}/libnx/include"
+export LDFLAGS="${ARCH} -L${PORTLIBS_PREFIX}/lib -L${DEVKITPRO}/libnx/lib"
+export LIBS="-lnx"
+
 # forgive me father, for I have sinned
 ln -s /usr/bin/python3 /usr/bin/python
-
-source $DEVKITPRO/switchvars.sh
 
 echo "Building libsolder..."
 
