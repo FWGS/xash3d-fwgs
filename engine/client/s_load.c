b6347d17c936dc40c5308ff09647e4459d691e39
Alibek Omarov <a1ba.omarov@gmail.com>
2023-02-19 12:25:03 +0300

    engine: client: sound: don't print sound/ twice in soundlist command

--- engine/client/s_load.c
+++ engine/client/s_load.c
@@ -54,7 +54,7 @@ void S_SoundList_f( void )
 
 			if( sc->loopStart >= 0 ) Con_Printf( "L" );
 			else Con_Printf( " " );
-			if( sfx->name[0] == '*' )
+			if( sfx->name[0] == '*' || !Q_strncmp( sfx->name, DEFAULT_SOUNDPATH, sizeof( DEFAULT_SOUNDPATH ) - 1 ))
 				Con_Printf( " (%2db) %s : %s\n", sc->width * 8, Q_memprint( sc->size ), sfx->name );
 			else Con_Printf( " (%2db) %s : " DEFAULT_SOUNDPATH "%s\n", sc->width * 8, Q_memprint( sc->size ), sfx->name );
 			totalSfx++;
