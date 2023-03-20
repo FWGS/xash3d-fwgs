1df1fc32dfcb7cbe790e5238987ceab1f9a97464
Alibek Omarov <a1ba.omarov@gmail.com>
2023-03-20 16:51:13 +0300

    scripts: gha: psvita: lock vitaGL revision, use --depth=1 in git clone

--- scripts/gha/deps_psvita.sh
+++ scripts/gha/deps_psvita.sh
@@ -6,12 +6,14 @@ echo "Downloading vitasdk..."
 
 export VITASDK=/usr/local/vitasdk
 
+VITAGL_SRCREV="c52391378c2bf1a00a0194c4fd88c35492d104b8" # lock vitaGL version to avoid compilation errors
+
 install_package()
 {
 	./vdpm $1 || exit 1
 }
 
-git clone https://github.com/vitasdk/vdpm.git || exit 1
+git clone https://github.com/vitasdk/vdpm.git --depth=1 || exit 1
 pushd vdpm
 ./bootstrap-vitasdk.sh || exit 1
 install_package taihen
@@ -24,18 +26,17 @@ popd
 
 echo "Downloading vitaGL..."
 
-git clone https://github.com/Rinnegatamante/vitaGL.git || exit 1
+git clone https://github.com/Rinnegatamante/vitaGL.git -b "$VITAGL_SRCREV" --depth=1 || exit 1
 
 echo "Downloading vitaGL fork of SDL2..."
 
-git clone https://github.com/Northfear/SDL.git || exit 1
+git clone https://github.com/Northfear/SDL.git --depth=1 || exit 1
 
 echo "Downloading vita-rtld..."
 
-git clone https://github.com/fgsfdsfgs/vita-rtld.git || exit 1
+git clone https://github.com/fgsfdsfgs/vita-rtld.git --depth=1 || exit 1
 
 echo "Downloading HLSDK..."
 
 rm -rf hlsdk-xash3d hlsdk-portable
-# TODO: change to FWGS/hlsdk-portable.git when changes are merged in
-git clone --recursive https://github.com/fgsfdsfgs/hlsdk-xash3d || exit 1
+git clone --recursive https://github.com/FWGS/hlsdk-portable || exit 1
