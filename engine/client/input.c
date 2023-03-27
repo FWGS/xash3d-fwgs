9a42f4149ff67f54258e778482f77b77083556cd
Alibek Omarov <a1ba.omarov@gmail.com>
2023-03-27 17:18:09 +0300

    engine: client: disable enabling mouse cursor in key_message (typing in chat)

--- engine/client/input.c
+++ engine/client/input.c
@@ -176,7 +176,7 @@ void IN_ToggleClientMouse( int newstate, int oldstate )
 
 	// since SetCursorType controls cursor visibility
 	// execute it first, and then check mouse grab state
-	if( newstate == key_menu || newstate == key_console || newstate == key_message )
+	if( newstate == key_menu || newstate == key_console )
 	{
 		Platform_SetCursorType( dc_arrow );
 
