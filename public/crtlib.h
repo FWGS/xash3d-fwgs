e0a11f95721ca8264b46729e38d8dfad53db8ea8
Alibek Omarov <a1ba.omarov@gmail.com>
2023-04-23 18:31:08 +0300

    public: crtlib: remove unused Q_strlwr macro

--- public/crtlib.h
+++ public/crtlib.h
@@ -59,7 +59,6 @@ const char *Q_buildcommit( void );
 //
 // crtlib.c
 //
-#define Q_strlwr( in, out ) Q_strnlwr( in, out, 99999 )
 void Q_strnlwr( const char *in, char *out, size_t size_out );
 #define Q_strlen( str ) (( str ) ? strlen(( str )) : 0 )
 size_t Q_colorstr( const char *string );
