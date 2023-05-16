dc5e4a39397809176af61b9597aec799cf88437c
Alibek Omarov <a1ba.omarov@gmail.com>
2023-05-16 23:44:52 +0300

    ref: gl: track current bound texture index in gl_textures array in separate glState field

--- ref/gl/gl_rsurf.c
+++ ref/gl/gl_rsurf.c
@@ -803,7 +803,7 @@ void DrawGLPoly( glpoly_t *p, float xScale, float yScale )
 			flConveyorSpeed = (e->curstate.rendercolor.g<<8|e->curstate.rendercolor.b) / 16.0f;
 			if( e->curstate.rendercolor.r ) flConveyorSpeed = -flConveyorSpeed;
 		}
-		texture = R_GetTexture( glState.currentTextures[glState.activeTMU] );
+		texture = R_GetTexture( glState.currentTexturesIndex[glState.activeTMU] );
 
 		flRate = fabs( flConveyorSpeed ) / (float)texture->srcWidth;
 		flAngle = ( flConveyorSpeed >= 0 ) ? 180 : 0;
