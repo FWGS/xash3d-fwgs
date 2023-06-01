bcc73d3dd0100733dd7366f7572d35402322acf6
Alibek Omarov <a1ba.omarov@gmail.com>
2023-06-02 00:28:17 +0300

    contrib: delete outdated files

--- contrib/mittorn/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCRIPTDIR=$(dirname "$0")
-TOPDIR=$SCRIPTDIR/../../
-cp $SCRIPTDIR/Makefile.dllloader $TOPDIR/loader/
-cp $SCRIPTDIR/Makefile.emscripten $TOPDIR/engine/
-cp $SCRIPTDIR/Makefile.linux $TOPDIR/engine/
-cp $SCRIPTDIR/wscript $TOPDIR/engine/
