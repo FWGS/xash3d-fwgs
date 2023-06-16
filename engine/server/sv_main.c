9d418dbea8185531f0abf1bc4a688d1143864350
Alibek Omarov <a1ba.omarov@gmail.com>
2023-06-16 07:19:17 +0300

    engine: server: add sv_log_outofband cvar that controls whether connectionless packets should be printed or not

--- engine/server/sv_main.c
+++ engine/server/sv_main.c
@@ -145,6 +145,7 @@ CVAR_DEFINE_AUTO( sv_userinfo_penalty_time, "0.3", FCVAR_ARCHIVE, "initial penal
 CVAR_DEFINE_AUTO( sv_userinfo_penalty_multiplier, "2", FCVAR_ARCHIVE, "penalty time multiplier" );
 CVAR_DEFINE_AUTO( sv_userinfo_penalty_attempts, "4", FCVAR_ARCHIVE, "if max attempts count was exceeded, penalty time will be increased" );
 CVAR_DEFINE_AUTO( sv_fullupdate_penalty_time, "1", FCVAR_ARCHIVE, "allow fullupdate command only once in this timewindow (set 0 to disable)" );
+CVAR_DEFINE_AUTO( sv_log_outofband, "0", FCVAR_ARCHIVE, "log out of band messages, can be useful for server admins and for engine debugging" );
 
 //============================================================================
 /*
@@ -939,6 +940,7 @@ void SV_Init( void )
 	Cvar_RegisterVariable( &sv_userinfo_penalty_multiplier );
 	Cvar_RegisterVariable( &sv_userinfo_penalty_attempts );
 	Cvar_RegisterVariable( &sv_fullupdate_penalty_time );
+	Cvar_RegisterVariable( &sv_log_outofband );
 
 	// when we in developer-mode automatically turn cheats on
 	if( host_developer.value ) Cvar_SetValue( "sv_cheats", 1.0f );
