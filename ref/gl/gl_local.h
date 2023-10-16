eff033298ac942824d99d4d6d33e11170ded420a
mittorn <mittorn@disroot.org>
2023-10-16 21:32:13 +0300

    ref_gl: disable singlepass detail renderer, use correct fog color multipliers in VBO

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -774,6 +774,7 @@ extern convar_t	r_lockfrustum;
 extern convar_t	r_traceglow;
 extern convar_t	r_vbo;
 extern convar_t	r_vbo_dlightmode;
+extern convar_t	r_vbo_detail;
 extern convar_t r_studio_sort_textures;
 extern convar_t r_studio_drawelements;
 extern convar_t r_ripple;
