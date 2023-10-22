90ed8c4b40b674f8b69a34aef82b0f1b00ea9745
Xav101 <xavieraleister694@gmail.com>
2023-10-22 12:00:00 -0500

    public: isnan fix for when using GCC on IRIX

--- public/xash3d_mathlib.h
+++ public/xash3d_mathlib.h
@@ -80,8 +80,9 @@ GNU General Public License for more details.
 #define Q_round( x, y )	(floor( x / y + 0.5f ) * y )
 #define Q_rint(x)		((x) < 0.0f ? ((int)((x)-0.5f)) : ((int)((x)+0.5f)))
 
-#ifdef XASH_IRIX
+#if defined( XASH_IRIX ) && defined( __GNUC__ )
 #undef isnan
+#define isnan __builtin_isnan
 #endif
 #ifdef isnan // check for C99 isnan
 #define IS_NAN isnan
