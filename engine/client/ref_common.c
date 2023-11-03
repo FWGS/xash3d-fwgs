0aa8a2891cb52668ad62f6795fa5b26aea0df683
Alibek Omarov <a1ba.omarov@gmail.com>
2023-11-03 17:03:27 +0300

    engine: client: add gles3compat to renderers list

--- engine/client/ref_common.c
+++ engine/client/ref_common.c
@@ -569,6 +569,9 @@ static void R_CollectRendererNames( void )
 #if XASH_REF_GL4ES_ENABLED
 		"gl4es",
 #endif
+#if XASH_REF_GLES3COMPAT_ENABLED
+		"gles3compat",
+#endif
 #if XASH_REF_SOFT_ENABLED
 		"soft",
 #endif
@@ -588,6 +591,9 @@ static void R_CollectRendererNames( void )
 #endif
 #if XASH_REF_GL4ES_ENABLED
 		"GL4ES",
+#endif
+#if XASH_REF_GLES3COMPAT_ENABLED
+		"GLES3 (gl2_shim)"
 #endif
 #if XASH_REF_SOFT_ENABLED
 		"Software",
