acdf1da995e1e63a497ee107ce4bc976409d7813
Alibek Omarov <a1ba.omarov@gmail.com>
2023-12-28 22:19:44 +0300

    ref: soft: remove GetMoveVars() call

--- ref/soft/r_studio.c
+++ ref/soft/r_studio.c
@@ -1320,7 +1320,7 @@ R_StudioDynamicLight
 */
 void R_StudioDynamicLight( cl_entity_t *ent, alight_t *plight )
 {
-	movevars_t	*mv = gEngfuncs.pfnGetMoveVars();
+	movevars_t	*mv = tr.movevars;
 	vec3_t		lightDir, vecSrc, vecEnd;
 	vec3_t		origin, dist, finalLight;
 	float		add, radius, total;
