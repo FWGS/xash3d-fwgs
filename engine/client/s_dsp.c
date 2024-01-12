6acc97b0ef8cb65e911a0c76c88a600f065fd6b9
Alibek Omarov <a1ba.omarov@gmail.com>
2024-01-13 00:56:03 +0300

    engine: client: restore my copyright on my DSP implementation from old engine branch

--- engine/client/s_dsp.c
+++ engine/client/s_dsp.c
@@ -1,6 +1,7 @@
 /*
 s_dsp.c - digital signal processing algorithms for audio FX
 Copyright (C) 2009 Uncle Mike
+Copyright (C) 2016-2024 Alibek Omarov
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
