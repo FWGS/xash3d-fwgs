bdc28530c0d529b1af958f9da2d17258a75ef2b0
relt-1 <60782515+relt-1@users.noreply.github.com>
2024-01-24 13:46:15 +0100

    max is missing for some reason

--- engine/common/imagelib/img_vtf.c
+++ engine/common/imagelib/img_vtf.c
@@ -1,6 +1,9 @@
 #include "img_vtf.h"
 #include "imagelib.h"
 
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
 
 qboolean Image_LoadVTF(const char* name, const byte* buffer, fs_offset_t filesize)
 {
