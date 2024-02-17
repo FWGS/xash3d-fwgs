05ff0d6020860b970dcf9b60f4d4abebc620c2da
Alibek Omarov <a1ba.omarov@gmail.com>
2024-02-17 22:17:22 +0300

    engine: cmd: remove unused global variable

--- engine/common/cmd.c
+++ engine/common/cmd.c
@@ -529,7 +529,6 @@ typedef struct cmd_s
 static int		cmd_argc;
 static const char	*cmd_args = NULL;
 static char		*cmd_argv[MAX_CMD_TOKENS];
-static char		cmd_tokenized[MAX_CMD_BUFFER];	// will have 0 bytes inserted
 static cmd_t		*cmd_functions;			// possible commands to execute
 
 /*
