f6625d91887f06d5b57fd89e86c2dfc28b04f8cd
Alibek Omarov <a1ba.omarov@gmail.com>
2024-05-30 07:22:21 +0300

    engine: common: remove unused et_name array

--- engine/common/world.c
+++ engine/common/world.c
@@ -20,16 +20,6 @@ GNU General Public License for more details.
 #include "xash3d_mathlib.h"
 #include "studio.h"
 
-// just for debug
-const char *et_name[] =
-{
-	"normal",
-	"player",
-	"tempentity",
-	"beam",
-	"fragmented",
-};
-
 /*
 ===============================================================================
 
