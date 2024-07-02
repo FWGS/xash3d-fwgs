c8469071bfd6494af2159be4293cf32b6363f056
Alibek Omarov <a1ba.omarov@gmail.com>
2024-07-02 19:15:02 +0300

    scripts: gha: add remaining wayland dependencies

--- scripts/gha/deps_linux.sh
+++ scripts/gha/deps_linux.sh
@@ -7,17 +7,17 @@ cd $GITHUB_WORKSPACE
 if [ "$GH_CPU_ARCH" == "i386" ]; then
 	sudo dpkg --add-architecture i386
 	sudo apt update
-	sudo apt install gcc-multilib g++-multilib libx11-dev:i386 libxext-dev:i386 x11-utils libgl1-mesa-dev libasound-dev libstdc++6:i386 libfuse2:i386 zlib1g:i386 libpulse0:i386 libpulse-dev libjack-dev:i386 libwayland-dev:i386
+	sudo apt install gcc-multilib g++-multilib libx11-dev:i386 libxext-dev:i386 x11-utils libgl1-mesa-dev libasound-dev libstdc++6:i386 libfuse2:i386 zlib1g:i386 libpulse0:i386 libpulse-dev libjack-dev:i386 libwayland-dev:i386 libxkbcommon-dev:i386 wayland-scanner++
 
 	wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-i686.AppImage" -O appimagetool.AppImage
 elif [ "$GH_CPU_ARCH" == "amd64" ]; then
 	sudo apt update
-	sudo apt install libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g libpulse-dev libjack-dev libwayland-dev
+	sudo apt install libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g libpulse-dev libjack-dev libwayland-dev libxkbcommon-dev wayland-scanner++
 
 	wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool.AppImage
 elif [ "$GH_CPU_ARCH" == "aarch64" ]; then
 	sudo apt update
-	sudo apt install libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g libpulse-dev libjack-dev libwayland-dev
+	sudo apt install libx11-dev libxext-dev x11-utils libgl1-mesa-dev libasound-dev libstdc++6 libfuse2 zlib1g libpulse-dev libjack-dev libwayland-dev libxkbcommon-dev wayland-scanner++
 
 	wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-aarch64.AppImage" -O appimagetool.AppImage
 else
