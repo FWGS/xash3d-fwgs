76978bfd47d6b8ad616397caaded4305c3b772c4
Alibek Omarov <a1ba.omarov@gmail.com>
2024-07-09 08:56:14 +0300

    scripts: gha: change Android project URL, update Android CLI tools and NDK

--- scripts/gha/deps_android.sh
+++ scripts/gha/deps_android.sh
@@ -2,6 +2,11 @@
 
 cd $GITHUB_WORKSPACE
 
+ANDROID_COMMANDLINE_TOOLS_VER="11076708"
+ANDROID_BUILD_TOOLS_VER="34.0.0"
+ANDROID_PLATFORM_VER="android-34"
+ANDROID_NDK_VER="26.3.11579264"
+
 echo "Download JDK 17"
 wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz -qO jdk.tar.gz || exit 1
 tar -xzf jdk.tar.gz
@@ -14,7 +19,7 @@ git clone --depth 1 --recursive https://github.com/FWGS/hlsdk-portable -b mobile
 echo "Download Android SDK"
 mkdir -p sdk || exit 1
 pushd sdk
-wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -qO sdk.zip || exit 1
+wget https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_COMMANDLINE_TOOLS_VER}_latest.zip -qO sdk.zip || exit 1
 unzip -q sdk.zip || exit 1
 mv cmdline-tools tools
 mkdir -p cmdline-tools
@@ -26,10 +31,10 @@ popd
 
 echo "Download all needed tools and Android NDK"
 yes | sdkmanager --licenses > /dev/null 2>/dev/null # who even reads licenses? :)
-sdkmanager --install build-tools\;34.0.0 platform-tools platforms\;android-34 ndk\;26.0.10792818
+sdkmanager --install build-tools\;${ANDROID_BUILD_TOOLS_VER} platform-tools platforms\;${ANDROID_PLATFORM_VER} ndk\;${ANDROID_NDK_VER}
 
 echo "Download Xash3D FWGS Android source code"
-git clone --depth 1 --recursive https://github.com/Velaron/xash3d-android-project -b gradle android || exit 1
+git clone --depth 1 --recursive https://github.com/FWGS/xash3d-android-project -b gradle android || exit 1
 pushd android/app/src/main/cpp
 
 mv xash3d-fwgs xash3d-fwgs-sub
