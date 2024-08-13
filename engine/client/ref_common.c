69ec810ed012b160d5c24b673c6b9b39f7bdde21
Alibek Omarov <a1ba.omarov@gmail.com>
2024-08-13 19:16:59 +0300

    engine: client: add missing ref_null to R_CollectRendererNames

--- engine/client/ref_common.c
+++ engine/client/ref_common.c
@@ -635,6 +635,9 @@ static void R_CollectRendererNames( void )
 #if XASH_REF_SOFT_ENABLED
 		"soft",
 #endif
+#if XASH_REF_NULL_ENABLED
+		"null",
+#endif
 	};
 
 	// ordering is important here too!
@@ -657,6 +660,9 @@ static void R_CollectRendererNames( void )
 #endif
 #if XASH_REF_SOFT_ENABLED
 		"Software",
+#endif
+#if XASH_REF_NULL_ENABLED
+		"Null Renderer",
 #endif
 	};
 
