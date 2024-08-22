4118ac2561d0a331609a9b250e0438f8f9a1c7a6
Alibek Omarov <a1ba.omarov@gmail.com>
2024-08-22 21:42:04 +0300

    ref: gl: add cvar gl_litwater to enable lightmapped water

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -749,6 +749,7 @@ extern convar_t	gl_test;		// cvar to testify new effects
 extern convar_t	gl_msaa;
 extern convar_t	gl_stencilbits;
 extern convar_t	gl_overbright;
+extern convar_t	gl_litwater;
 
 extern convar_t	r_lighting_extended;
 extern convar_t	r_lighting_ambient;
