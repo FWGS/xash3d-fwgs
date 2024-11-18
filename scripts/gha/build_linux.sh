bfb84a7ac4b33529bc3c715bdb508e62416120e6
Alibek Omarov <a1ba.omarov@gmail.com>
2024-11-18 16:53:47 +0300

    ci: rework Linux build scripts, allow crosscompiling with whatever is included in Ubuntu 20.04 (which is used on GitHub Actions now) repositories

--- scripts/gha/build_linux.sh
+++ scripts/gha/build_linux.sh
@@ -2,46 +2,49 @@
 
 . scripts/lib.sh
 
+# "booo, bash feature!"
+declare -A ARCH_TRIPLET CROSS_COMPILE_CC CROSS_COMPILE_CXX
+ARCH_TRIPLET[amd64]=x86_64-linux-gnu
+ARCH_TRIPLET[i386]=i386-linux-gnu
+ARCH_TRIPLET[arm64]=aarch64-linux-gnu
+ARCH_TRIPLET[armhf]=arm-linux-gnueabihf
+ARCH_TRIPLET[riscv64]=riscv64-linux-gnu
+ARCH_TRIPLET[ppc64el]=powerpc64le-linux-gnu
+CROSS_COMPILE_CC[amd64]=cc
+CROSS_COMPILE_CC[i386]="cc -m32"
+CROSS_COMPILE_CXX[amd64]=c++
+CROSS_COMPILE_CXX[i386]="c++ -m32"
+for i in arm64 armhf riscv64 ppc64el; do
+	CROSS_COMPILE_CC[$i]=${ARCH_TRIPLET[$i]}-gcc
+	CROSS_COMPILE_CXX[$i]=${ARCH_TRIPLET[$i]}-g++
+done
+export PKG_CONFIG_PATH=${ARCH_TRIPLET[$GH_CPU_ARCH]}
+export CC=${CROSS_COMPILE_CC[$GH_CPU_ARCH]}
+export CXX=${CROSS_COMPILE_CXX[$GH_CPU_ARCH]}
+
 APP=xash3d-fwgs
 APPDIR=$APP.AppDir
 APPIMAGE=$APP-$ARCH.AppImage
 
+APPDIR2=$APP-linux-$ARCH # FIXME: not conforms to libpublic's build arch strings but in parity with xashds directory name
+APPTARGZ=$APP-linux-$ARCH.tar.gz
+
 DS=xashds-linux
 DSDIR=$DS-$ARCH
 DSTARGZ=$DS-$ARCH.tar.gz
+N=$(nproc)
 
 build_sdl2()
 {
 	cd "$BUILDDIR"/SDL2_src || die
-	if [ "$ARCH" = "i386" ]; then
-		export CFLAGS="-msse2 -march=i686 -m32 -ggdb -O2"
-		export LDFLAGS="-m32"
-		export PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig"
-	fi
 
-	# TODO: enable pipewire after we migrate from 20.04
-	./configure \
-		--disable-render \
-		--disable-haptic \
-		--disable-power \
-		--disable-filesystem \
-		--disable-file \
-		--disable-libudev \
-		--disable-dbus \
-		--disable-ibus \
-		--disable-ime \
-		--disable-fcitx \
-		--enable-alsa-shared \
-		--enable-jack-shared \
-		--enable-pulseaudio-shared \
-		--enable-wayland-shared \
-		--enable-x11-shared \
-		--prefix / || die # get rid of /usr/local stuff
-	make -j2 || die
-	mkdir -p "$BUILDDIR"/SDL2_linux
-	make install DESTDIR="$BUILDDIR"/SDL2_linux || die
-	export CFLAGS=""
-	export LDFLAGS=""
+	# a1ba: let's make something different. Rather than removing features
+	# let's enable everything we can
+	mkdir -p build || die
+	pushd build || die
+		cmake ../ -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$BUILDDIR"/SDL2_linux -DCMAKE_C_FLAGS=-O3 -DSDL_STATIC=OFF || die
+		ninja install -j$((N+1)) || die
+	popd || die
 }
 
 build_engine()
@@ -53,10 +56,14 @@ build_engine()
 		AMD64="-8"
 	fi
 
+	if [ "$GH_CROSSCOMPILING" != "true" ]; then
+		ENABLE_TESTS="--enable-tests"
+	fi
+
 	if [ "$1" = "dedicated" ]; then
-		./waf configure -T release -d $AMD64 --enable-tests --enable-lto --enable-bundled-deps || die_configure
+		./waf configure $AMD64 $ENABLE_TESTS --enable-lto --enable-bundled-deps -d || die_configure
 	elif [ "$1" = "full" ]; then
-		./waf configure --sdl2=SDL2_linux -T release --enable-stb $AMD64 --enable-utils --enable-tests --enable-lto --enable-bundled-deps || die_configure
+		./waf configure $AMD64 $ENABLE_TESTS --enable-lto --enable-bundled-deps -s SDL2_linux --enable-stb --enable-utils || die_configure
 	else
 		die
 	fi
@@ -64,57 +71,43 @@ build_engine()
 	./waf build || die_configure
 }
 
-build_appimage()
+deploy_engine()
 {
 	cd "$BUILDDIR" || die
-
 	./waf install --destdir="$APPDIR" || die
-
 	cp SDL2_linux/lib/libSDL2-2.0.so.0 "$APPDIR/"
-	if [ "$ARCH" = "i386" ]; then
+	if [ "$GH_CPU_ARCH" = "i386" ]; then
 		cp 3rdparty/vgui_support/vgui-dev/lib/vgui.so "$APPDIR/"
 	fi
+}
 
-	cat > "$APPDIR"/AppRun << 'EOF'
-#!/bin/sh
+build_appimage()
+{
+	deploy_engine
 
-if [ "$XASH3D_BASEDIR" = "" ]; then
-	export XASH3D_BASEDIR=$PWD
-fi
-echo "Xash3D FWGS installed as AppImage."
-echo "Base directory is $XASH3D_BASEDIR. Set XASH3D_BASEDIR environment variable to override this"
-
-export XASH3D_EXTRAS_PAK1="${APPDIR}"/valve/extras.pk3
-${DEBUGGER} "${APPDIR}"/xash3d "$@"
-exit $?
-EOF
-
-	chmod +x "$APPDIR"/xash3d "$APPDIR"/AppRun # Engine launcher & engine launcher script
-
-	echo "Contents of AppImage: "
-	ls -R "$APPDIR"
-
+	cp scripts/gha/linux/AppRun "$APPDIR/AppRun"
+	cp scripts/gha/linux/xash3d-fwgs.desktop "$APPDIR/$APP.desktop"
 	wget "https://raw.githubusercontent.com/FWGS/fwgs-artwork/master/xash3d/icon_512.png" -O "$APPDIR/$APP.png"
 
-	cat > "$APPDIR/$APP.desktop" <<EOF
-[Desktop Entry]
-Name=xash3d-fwgs
-Icon=xash3d-fwgs
-Type=Application
-Exec=AppRun
-Categories=Game;
-EOF
+	chmod +x "$APPDIR"/AppRun # Engine launcher & engine launcher script
+	echo "Contents of AppImage: "
+	ls -R "$APPDIR"
+	./appimagetool.AppImage "$APPDIR" "artifacts/$APPIMAGE"
+}
 
-	./appimagetool.AppImage "$APPDIR" "$APPIMAGE"
+build_engine_tarball()
+{
+	deploy_engine
+
+	mv "$APPDIR" "$APPDIR2"
+	tar -czvf "artifacts/$APPTARGZ" "$APPDIR2"
 }
 
 build_dedicated_tarball()
 {
 	cd "$BUILDDIR" || die
-
-	./waf install --destdir=$DSDIR || die
-
-	tar -czvf $DSTARGZ $DSDIR
+	./waf install --destdir="$DSDIR" || die
+	tar -czvf "artifacts/$DSTARGZ" "$DSDIR"
 }
 
 mkdir -p artifacts/
@@ -122,9 +115,12 @@ mkdir -p artifacts/
 rm -rf build # clean-up build directory
 build_engine dedicated
 build_dedicated_tarball
-mv $DSTARGZ artifacts/
 
 build_sdl2
 build_engine full # don't rebuild some common parts twice
-build_appimage
-mv $APPIMAGE artifacts/
+
+if [ -x appimagetool.AppImage ]; then
+	build_appimage
+else
+	build_engine_tarball
+fi
