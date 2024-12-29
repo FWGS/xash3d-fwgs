08c2632c14bf27e85ce46907649b8422377674d0
Alibek Omarov <a1ba.omarov@gmail.com>
2024-12-29 09:05:38 +0300

    engine: platform: nswitch: export _Unwind_GetIPInfo as required by cs16-client

--- engine/platform/nswitch/sys_nswitch.c
+++ engine/platform/nswitch/sys_nswitch.c
@@ -34,6 +34,7 @@ long pathconf( const char *path, int name ) { return -1; }
 
 // part of libunwind; required by any dynamic lib that uses C++ exceptions
 extern void *_Unwind_Resume;
+extern void *_Unwind_GetIPInfo;
 
 // these are macros in our libc, so we need to wrap them
 static int tolower_fn( int c ) { return tolower( c ); }
@@ -55,6 +56,7 @@ static const solder_export_t aux_exports[] =
 	SOLDER_EXPORT_SYMBOL( strchrnul ),
 	SOLDER_EXPORT_SYMBOL( stpcpy ),
 	SOLDER_EXPORT_SYMBOL( _Unwind_Resume ),
+	SOLDER_EXPORT_SYMBOL( _Unwind_GetIPInfo ),
 };
 
 const solder_export_t *__solder_aux_exports = aux_exports;
