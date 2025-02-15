b605cf603e7f0c9f008686dc30e63f2978f63746
Alibek Omarov <a1ba.omarov@gmail.com>
2025-02-15 05:33:19 +0300

    common: xash3d_types: make it more compatible for inclusion in SDK

--- common/xash3d_types.h
+++ common/xash3d_types.h
@@ -13,13 +13,19 @@
 #endif // _WIN32
 
 #include <sys/types.h> // off_t
+#ifdef STDINT_H
 #include STDINT_H
+#else // !STDINT_H
+#include <stdint.h>
+#endif // !STDINT_H
 #include <assert.h>
 
 typedef uint8_t  byte;
 typedef float    vec_t;
 typedef vec_t    vec2_t[2];
+#ifndef vec3_t // SDK renames it to Vector
 typedef vec_t    vec3_t[3];
+#endif
 typedef vec_t    vec4_t[4];
 typedef vec_t    quat_t[4];
 typedef byte     rgba_t[4];	// unsigned byte colorpack
@@ -44,6 +50,7 @@ typedef int qboolean;
 
 #define BIT( n )		( 1U << ( n ))
 #define BIT64( n )		( 1ULL << ( n ))
+
 #define SetBits( iBitVector, bits )	((iBitVector) = (iBitVector) | (bits))
 #define ClearBits( iBitVector, bits )	((iBitVector) = (iBitVector) & ~(bits))
 #define FBitSet( iBitVector, bit )	((iBitVector) & (bit))
@@ -59,6 +66,8 @@ typedef int qboolean;
 // color strings
 #define IsColorString( p )	( p && *( p ) == '^' && *(( p ) + 1) && *(( p ) + 1) >= '0' && *(( p ) + 1 ) <= '9' )
 #define ColorIndex( c )	((( c ) - '0' ) & 7 )
+
+#undef EXPORT
 
 #if defined( __GNUC__ )
 	#if defined( __i386__ )
