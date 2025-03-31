6cf0d7a8714b924f9fd704ae024e8388fd4da3fa
Alibek Omarov <a1ba.omarov@gmail.com>
2025-03-31 18:20:23 +0300

    scripts: gha: linux: fix enabling stb_truetype in mainui_cpp

--- scripts/gha/build_linux.sh
+++ scripts/gha/build_linux.sh
@@ -63,7 +63,7 @@ build_engine()
 	if [ "$1" = "dedicated" ]; then
 		./waf configure $AMD64 $ENABLE_TESTS --enable-lto --enable-bundled-deps -d || die_configure
 	elif [ "$1" = "full" ]; then
-		./waf configure $AMD64 $ENABLE_TESTS --enable-lto --enable-bundled-deps -s SDL2_linux --enable-stb --enable-utils || die_configure
+		./waf configure $AMD64 $ENABLE_TESTS --enable-lto --enable-bundled-deps -s SDL2_linux --enable-stbtt --enable-utils || die_configure
 	else
 		die
 	fi
