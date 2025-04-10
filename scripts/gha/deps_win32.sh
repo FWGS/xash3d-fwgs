a20e961892e7a89bcaa73eae21175ade57edcda6
Alibek Omarov <a1ba.omarov@gmail.com>
2025-04-10 22:02:13 +0300

    scripts: gha: install i686-pc-windows-msvc rust target for cross-compiling 32-bit maintui

--- scripts/gha/deps_win32.sh
+++ scripts/gha/deps_win32.sh
@@ -3,3 +3,7 @@
 curl -L https://github.com/libsdl-org/SDL/releases/download/release-$SDL_VERSION/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
+
+if [ "$GH_CPU_ARCH" = "i386" ]; then
+	rustup target add i686-pc-windows-msvc
+fi
