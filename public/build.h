6aa281bc59f9559cf959884d54ae34c127b60dbb
Alibek Omarov <a1ba.omarov@gmail.com>
2025-06-14 15:26:05 +0500

    public: define Emscripten as POSIX-compatible

--- public/build.h
+++ public/build.h
@@ -97,8 +97,6 @@ Then you can use another oneliner to query all variables:
 //================================================================
 #if defined _WIN32
 	#define XASH_WIN32 1
-#elif defined __EMSCRIPTEN__
-	#define XASH_EMSCRIPTEN 1
 #elif defined __WATCOMC__ && defined __DOS__
 	#define XASH_DOS4GW 1
 #else // POSIX compatible
@@ -137,6 +135,8 @@ Then you can use another oneliner to query all variables:
 		#define XASH_WASI 1
 	#elif defined __sun__
 		#define XASH_SUNOS 1
+	#elif defined __EMSCRIPTEN__
+		#define XASH_EMSCRIPTEN 1
 	#else
 		#error
 	#endif
