fc368c336ff0d12e7d45bfcfcb9ccb4653da1b29
Bohdan Shulyar <mars873@gmail.com>
2025-08-03 17:10:02 +0300

    android: initial new frontend

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
 	alias(libs.plugins.android.application) apply false
 	alias(libs.plugins.kotlin.android) apply false
+	alias(libs.plugins.compose.compiler) apply false
+	alias(libs.plugins.google.services) apply false
+	alias(libs.plugins.firebase.crashlytics) apply false
+	alias(libs.plugins.kotlin.serialization) apply false
+	alias(libs.plugins.hilt.android) apply false
+	alias(libs.plugins.ksp) apply false
 }
