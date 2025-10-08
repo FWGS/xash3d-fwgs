8d7ee70e7d1e7719e64a14c0e57095750e939783
Alibek Omarov <a1ba.omarov@gmail.com>
2025-10-08 05:27:50 +0500

    engine: platform: android: include dlfcn.h for Android's LoadLibrary implementation

--- engine/platform/android/lib_android.c
+++ engine/platform/android/lib_android.c
@@ -12,6 +12,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
+#include <dlfcn.h>
 #include "common.h"
 #include "library.h"
 #include "filesystem.h"
