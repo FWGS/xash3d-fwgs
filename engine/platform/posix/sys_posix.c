c85c6f80e05953663b49e002b20d5533ce46d4a6
Xav101 <xavieraleister694@gmail.com>
2025-11-14 22:37:47 -0500

    platform: fix type in formatter string

--- engine/platform/posix/sys_posix.c
+++ engine/platform/posix/sys_posix.c
@@ -56,7 +56,7 @@ void Posix_Daemonize( void )
 		if( daemon > 0 )
 		{
 			// parent
-			Con_Reportf( "Child pid: %i\n", daemon );
+			Con_Reportf( "Child pid: %ld\n", (long)daemon );
 			exit( 0 );
 		}
 		else
