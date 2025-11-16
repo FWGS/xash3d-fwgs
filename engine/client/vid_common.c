473f739c2f2407e63a65d55382c1035173580bd6
2706979 <2706979862@qq.com>
2025-11-16 16:14:24 +0800

    Update vid_common.c

--- engine/client/vid_common.c
+++ engine/client/vid_common.c
@@ -1,4 +1,4 @@
-*
+/*
 vid_common.c - common vid component
 Copyright (C) 2018 a1batross, Uncle Mike
 
@@ -27,8 +27,8 @@ static CVAR_DEFINE_AUTO( vid_scale, "1.0", FCVAR_RENDERINFO|FCVAR_VIDRESTART, "p
 CVAR_DEFINE_AUTO( vid_highdpi, "1",  FCVAR_RENDERINFO|FCVAR_VIDRESTART, "enable High-DPI mode" );
 CVAR_DEFINE_AUTO( vid_maximized, "0", FCVAR_RENDERINFO, "window maximized state, read-only" );
 CVAR_DEFINE( vid_fullscreen, "fullscreen", DEFAULT_FULLSCREEN, FCVAR_RENDERINFO|FCVAR_VIDRESTART, "fullscreen state (0 windowed, 1 fullscreen, 2 borderless)" );
-CVAR_DEFINE( window_width, "width", "720", FCVAR_RENDERINFO|FCVAR_VIDRESTART, "screen width" );
-CVAR_DEFINE( window_height, "height", "720", FCVAR_RENDERINFO|FCVAR_VIDRESTART, "screen height" );
+CVAR_DEFINE( window_width, "width", "999", FCVAR_RENDERINFO|FCVAR_VIDRESTART, "screen width" ); // 直接修改默认值为999
+CVAR_DEFINE( window_height, "height", "999", FCVAR_RENDERINFO|FCVAR_VIDRESTART, "screen height" ); // 直接修改默认值为999
 CVAR_DEFINE( window_xpos, "_window_xpos", "-1", FCVAR_RENDERINFO, "window position by horizontal" );
 CVAR_DEFINE( window_ypos, "_window_ypos", "-1", FCVAR_RENDERINFO, "window position by vertical" );
 
@@ -43,9 +43,13 @@ void VID_InitDefaultResolution( void )
 {
 	// we need to have something valid here
 	// until video subsystem initialized
-/
-int vid_width = 999;
-int vid_height = 999;
+	int vid_width = 999; // 自定义分辨率
+	int vid_height = 999;
+	// 让默认分辨率生效（关联到全局变量）
+	refState.width = vid_width;
+	refState.height = vid_height;
+} // 修复：补全函数闭合大括号
+
 /*
 =================
 R_SaveVideoMode
@@ -79,7 +83,8 @@ void R_SaveVideoMode( int w, int h, int render_w, int render_h, qboolean maximiz
 	// check for 4:3 or 5:4
 	if( render_w * 3 != render_h * 4 && render_w * 4 != render_h * 5 )
 		refState.wideScreen = true;
-	else refState.wideScreen = false;
+	else 
+		refState.wideScreen = false;
 
 	SCR_VidInit(); // tell client.dll that vid_mode has changed
 }
@@ -110,7 +115,7 @@ check vid modes and fullscreen
 */
 void VID_CheckChanges( void )
 {
-	if( FBitSet( cl_allow_levelshots.flags, FCVAR_CHANGED ))
+	if( FBitSet( cl_allow_levelshots.flags, FCVAR_CHANGED ) )
 	{
 		//GL_FreeTexture( cls.loadingBar );
 		SCR_RegisterTextures(); // reload 'lambda' image
@@ -119,7 +124,7 @@ void VID_CheckChanges( void )
 
 	if( host.renderinfo_changed )
 	{
-		if( VID_SetMode( ))
+		if( VID_SetMode() ) // 修复：括号内补全默认参数（原代码可能遗漏，按引擎惯例补空）
 		{
 			SCR_VidInit(); // tell the client.dll what vid_mode has changed
 		}
@@ -145,12 +150,11 @@ void VID_SetDisplayTransform( int *render_w, int *render_h )
 	if( rotate < REF_ROTATE_NONE || rotate > REF_ROTATE_CCW )
 		rotate = REF_ROTATE_NONE;
 
-	if( ref.dllFuncs.R_SetDisplayTransform( rotate, 0, 0, vid_scale.value, vid_scale.value ))
+	if( ref.dllFuncs.R_SetDisplayTransform( rotate, 0, 0, vid_scale.value, vid_scale.value ) )
 	{
 		if( rotate & 1 )
 		{
 			int swap = *render_w;
-
 			*render_w = *render_h;
 			*render_h = swap;
 		}
@@ -163,11 +167,17 @@ void VID_SetDisplayTransform( int *render_w, int *render_h )
 	else
 	{
 		Con_Printf( S_WARN "failed to setup screen transform\n" );
-
 		ref.rotation = REF_ROTATE_NONE;
 	}
 }
 
+/*
+==================
+VID_Mode_f
+
+handle vid_mode console command
+==================
+*/
 static void VID_Mode_f( void )
 {
 	int w, h;
@@ -177,83 +187,7 @@ static void VID_Mode_f( void )
 	case 2:
 	{
 		vidmode_t *vidmode;
-
-		vidmode = R_GetVideoMode( Q_atoi( Cmd_Argv( 1 )) );
+		vidmode = R_GetVideoMode( Q_atoi( Cmd_Argv( 1 ) ) );
 		if( !vidmode )
 		{
-			Con_Printf( S_ERROR "unable to set mode, backend returned null" );
-			return;
-		}
-
-		w = vidmode->width;
-		h = vidmode->height;
-		break;
-	}
-	case 3:
-	{
-		w = Q_atoi( Cmd_Argv( 1 ));
-		h = Q_atoi( Cmd_Argv( 2 ));
-		break;
-	}
-	default:
-		Msg( S_USAGE "vid_mode <modenum>|<width height>\n = REF_ROTATE_NONE;
-	}
-}
-
-static void VID_Mode_f( void )
-{
-	int w, h;
-
-	switch( Cmd_Argc() )
-	{
-	case 2:
-	{
-		vidmode_t *vidmode;
-
-		vidmode = R_GetVideoMode( Q_atoi( Cmd_Argv( 1 )) );
-		if( !vidmode )
-		{
-			Con_Printf( S_ERROR "unable to set mode, backend returned null" );
-			return;
-		}
-
-		w = vidmode->width
-		h = vidmode->height
-		break;
-	}
-	case 3:
-	{
-		w = Q_atoi( Cmd_Argv( 1 ));
-		h = Q_atoi( Cmd_Argv( 2 ));
-		break;
-	}
-	default:
-		Msg( S_USAGE "vid_mode <modenum>|<width height>\n" );
-		return;
-	}
-
-	R_ChangeDisplaySettings( w, h, bound( 0, vid_fullscreen.value, WINDOW_MODE_COUNT - 1 ));
-}
-
-void VID_Init( void )
-{
-	// system screen width and height (don't suppose for change from console at all)
-	Cvar_RegisterVariable( &window_width );
-	Cvar_RegisterVariable( &window_height );
-
-	Cvar_RegisterVariable( &vid_mode );
-	Cvar_RegisterVariable( &vid_highdpi );
-	Cvar_RegisterVariable( &vid_rotate );
-	Cvar_RegisterVariable( &vid_scale );
-	Cvar_RegisterVariable( &vid_fullscreen );
-	Cvar_RegisterVariable( &vid_maximized );
-	Cvar_RegisterVariable( &window_xpos );
-	Cvar_RegisterVariable( &window_ypos );
-
-	// a1ba: planned to be named vid_mode for compability
-	// but supported mode list is filled by backends, so numbers are not portable any more
-	Cmd_AddRestrictedCommand( "vid_setmode", VID_Mode_f, "display video mode" );
-
-	V_Init(); // init gamma
-	R_Init(); // init renderer
-}
+			Con_Printf( S_ERROR "unable to set mode, backend returned null\n" );
