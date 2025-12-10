146244f1f42c69c26b1185f3cc8351789754cc2c
ksagameng <fordealisbad@gmail.com>
2025-12-10 12:57:03 +0300

    scripts: gha: Set hlsdk minimum ios version to 12.0

--- scripts/gha/build_ios.sh
+++ scripts/gha/build_ios.sh
@@ -9,7 +9,7 @@ cd $GITHUB_WORKSPACE || die
 cp -vr /Library/Frameworks/SDL2.framework ./build
 
 pushd hlsdk || die
-cmake -DCMAKE_SYSTEM_NAME=iOS -DGAMEDIR=$(realpath ../build/ios) -DCMAKE_BUILD_TYPE=Debug -B build -S .
+cmake -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 -DGAMEDIR=$(realpath ../build/ios) -DCMAKE_BUILD_TYPE=Debug -B build -S .
 cmake --build build --target install || die
 popd
 
