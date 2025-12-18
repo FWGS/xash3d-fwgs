414da98c21e3c6590e092a3783648df8410b3350
Alibek Omarov <a1ba.omarov@gmail.com>
2025-12-18 09:58:49 +0500

    Documentation: add example GPL copyright header

---
+++ Documentation/gpl_copyright_header.h
@@ -0,0 +1,15 @@
+/*
+filename.ext - do not include this file, only copy the header
+Copyright (C) 2015-2025 Xash3D FWGS contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
