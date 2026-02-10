306455280adc2cf6de180115d8a798d28a07b431
Alibek Omarov <a1ba.omarov@gmail.com>
2026-02-10 16:13:56 +0500

    engine: identification: include build.h early so platform definitions are visible when including system headers

--- engine/client/identification.c
+++ engine/client/identification.c
@@ -13,6 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
 
+#include "build.h"
 #include <inttypes.h>
 #include <fcntl.h>
 #if !XASH_WIN32
