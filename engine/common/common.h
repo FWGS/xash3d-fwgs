75fb6c5f34e4ea535e4fd22f3841d13075390fde
Alibek Omarov <a1ba.omarov@gmail.com>
2026-04-06 03:58:42 +0500

    engine: add Mem_Free2 that takes a pointer to pointer and then sets it to NULL

--- engine/common/common.h
+++ engine/common/common.h
@@ -381,6 +381,10 @@ void Mem_Stats_f( void );
 #define Mem_Calloc( pool, size ) _Mem_Alloc( pool, size, true, __FILE__, __LINE__ )
 #define Mem_Realloc( pool, ptr, size ) _Mem_Realloc( pool, ptr, size, true, __FILE__, __LINE__ )
 #define Mem_Free( mem ) _Mem_Free( mem, __FILE__, __LINE__ )
+#define Mem_Free2( ptr ) { \
+	_Mem_Free( *ptr, __FILE__, __LINE__ ); \
+	*ptr = NULL; }
+
 #define Mem_AllocPool( name ) _Mem_AllocPool( name, __FILE__, __LINE__ )
 #define Mem_FreePool( pool ) _Mem_FreePool( pool, __FILE__, __LINE__ )
 #define Mem_EmptyPool( pool ) _Mem_EmptyPool( pool, __FILE__, __LINE__ )
