6a3999f71d517975b35b3324ab6d52da773a5848
ksagameng <fordealisbad@gmail.com>
2026-04-08 18:11:22 +0300

    scripts: ios: Modify script to work with dylib loading changes

--- scripts/ios/buildallmods.sh
+++ scripts/ios/buildallmods.sh
@@ -5,41 +5,37 @@ SCRIPTDIR=${0%/*}
 cd "$SCRIPTDIR" || exit 1
 
 #shouldn't build any mods that are supported by mobile_hacks
-./buildhlsdk.sh aghl _ag
-./buildhlsdk.sh aomdc _AoMDC
-./buildhlsdk.sh asheep _asheep
-./buildhlsdk.sh blackops _blackops
-./buildhlsdk.sh bubblemod _bubblemod
-./buildhlsdk.sh CAd _CAd
-./buildhlsdk.sh cracklife _cracklife
-./buildhlsdk.sh clcampaign _clcampaign
-./buildhlsdk.sh dmc _dmc
-./buildhlsdk.sh eftd _eftd
-#linux only??
-./buildhlsdk.sh decay-pc _decay
-./buildhlsdk.sh echoes _echoes
-./buildhlsdk.sh gravgun _gravgun
-./buildhlsdk.sh opfor _opfor
-./buildhlsdk.sh sci _sci
-./buildhlsdk.sh topdown _topdown
-./buildhlsdk.sh half-screwed _half-screwed
-./buildhlsdk.sh noffice _noffice
-./buildhlsdk.sh poke646 _poke646
-./buildhlsdk.sh poke646_vendetta _poke646_vendetta
-./buildhlsdk.sh rebellion _rebellion
-./buildhlsdk.sh residual_point _rl
-./buildhlsdk.sh residual_point _rp_v1_pub_final1
-./buildhlsdk.sh sohl1.2 _Spirit
-./buildhlsdk.sh halloween _shall1
-./buildhlsdk.sh thegate _thegate
-./buildhlsdk.sh theyhunger _hunger
-./buildhlsdk.sh zombie-x _Zombie-X-DLE
+./buildhlsdk.sh aghl
+./buildhlsdk.sh aomdc
+./buildhlsdk.sh asheep
+./buildhlsdk.sh blackops
+./buildhlsdk.sh bubblemod
+./buildhlsdk.sh CAd
+./buildhlsdk.sh cracklife
+./buildhlsdk.sh clcampaign
+./buildhlsdk.sh dmc
+./buildhlsdk.sh eftd
+./buildhlsdk.sh decay-pc
+./buildhlsdk.sh echoes
+./buildhlsdk.sh gravgun
+./buildhlsdk.sh opfor
+./buildhlsdk.sh sci
+./buildhlsdk.sh topdown
+./buildhlsdk.sh half-screwed
+./buildhlsdk.sh noffice
+./buildhlsdk.sh poke646
+./buildhlsdk.sh poke646_vendetta
+./buildhlsdk.sh rebellion
+./buildhlsdk.sh residual_point
+./buildhlsdk.sh sohl1.2
+./buildhlsdk.sh halloween
+./buildhlsdk.sh thegate
+./buildhlsdk.sh theyhunger
+./buildhlsdk.sh zombie-x
+./buildhlsdk.sh delta_particles
 #untested if this works
 export XENWARRIOR=ON
-./buildhlsdk.sh sohl1.2 _xenwar
-
+./buildhlsdk.sh sohl1.2 xenwar
 ./buildhlsdk.sh 
 
-
-cd ../../
 ./createipa.sh
