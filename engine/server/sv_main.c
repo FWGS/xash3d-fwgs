a679c53111a587a7d7c551957c4dd0ec14b1d301
TheEVolk <elektro-volk@yandex.ru>
2026-04-09 22:14:12 +0300

    engine: client: add cvar sv_voice_singleplayer flag FCVAR_SERVER

--- engine/server/sv_main.c
+++ engine/server/sv_main.c
@@ -124,7 +124,7 @@ static CVAR_DEFINE_AUTO( violence_agibs, "1", 0, "show alien gib entities" );
 // voice chat
 CVAR_DEFINE_AUTO( sv_voiceenable, "1", FCVAR_ARCHIVE|FCVAR_SERVER, "enable voice support" );
 CVAR_DEFINE_AUTO( sv_voicequality, "3", FCVAR_ARCHIVE, "voice chat quality level, from 0 to 5, higher is better" );
-CVAR_DEFINE_AUTO( sv_voice_singleplayer, "0", FCVAR_ARCHIVE, "enable voice subsystem in singleplayer games (maxclients == 1)" );
+CVAR_DEFINE_AUTO( sv_voice_singleplayer, "0", FCVAR_ARCHIVE|FCVAR_SERVER, "enable voice subsystem in singleplayer games (maxclients == 1)" );
 
 // enttools
 CVAR_DEFINE_AUTO( sv_enttools_enable, "0", FCVAR_ARCHIVE|FCVAR_PROTECTED, "enable powerful and dangerous entity tools" );
