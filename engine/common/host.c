34e61eab5d81d2ac7c4e5b2e00f6530294ada446
hasandramali <93338836+hasandramali@users.noreply.github.com>
2026-05-05 13:40:08 +0300

    Update host.c

--- engine/common/host.c
+++ engine/common/host.c
@@ -74,7 +74,7 @@ static CVAR_DEFINE_AUTO( host_serverstate, "0", FCVAR_READ_ONLY, "displays curre
 static CVAR_DEFINE_AUTO( host_gameloaded, "0", FCVAR_READ_ONLY, "inidcates a loaded game.dll" );
 static CVAR_DEFINE_AUTO( host_clientloaded, "0", FCVAR_READ_ONLY, "inidcates a loaded client.dll" );
 CVAR_DEFINE_AUTO( host_limitlocal, "0", 0, "apply cl_cmdrate and rate to loopback connection" );
-CVAR_DEFINE( host_maxfps, "fps_max", "72", FCVAR_ARCHIVE|FCVAR_FILTERABLE, "host fps upper limit" );
+CVAR_DEFINE( host_maxfps, "max_fps", "72", FCVAR_ARCHIVE|FCVAR_PROTECTED, "host fps upper limit" );
 CVAR_DEFINE_AUTO( fps_override, "0", FCVAR_FILTERABLE, "unlock higher framerate values, not supported" );
 static CVAR_DEFINE_AUTO( host_framerate, "0", FCVAR_FILTERABLE, "locks frame timing to this value in seconds" );
 static CVAR_DEFINE( host_sleeptime, "sleeptime", "1", FCVAR_ARCHIVE|FCVAR_FILTERABLE, "milliseconds to sleep for each frame. higher values reduce fps accuracy" );
@@ -513,7 +513,7 @@ static double Host_CalcFPS( void )
 	{
 		fps = CL_GetDemoFramerate();
 	}
-	else if( Host_IsSinglePlayerGame( ))
+	else if( Host_IsLocalGame( ))
 	{
 		if( !gl_vsync.value )
 			fps = host_maxfps.value;
@@ -642,10 +642,9 @@ static qboolean Host_FilterTime( double time )
 	oldtime = host.realtime;
 
 	// NOTE: allow only in singleplayer while demos are not active
-	if( host_framerate.value > 0.0f && Host_IsSinglePlayerGame() && !CL_IsPlaybackDemo() && !CL_IsRecordDemo( ))
+	if( host_framerate.value > 0.0f )
 		host.frametime = bound( MIN_FRAMETIME, host_framerate.value * scale, MAX_FRAMETIME );
-	else
-		host.frametime = bound( MIN_FRAMETIME, host.frametime, MAX_FRAMETIME );
+	else host.frametime = bound( MIN_FRAMETIME, host.frametime, MAX_FRAMETIME );
 
 	return true;
 }
@@ -1026,7 +1025,7 @@ static void Host_InitCommon( int argc, char **argv, const char *progname, qboole
 		developer = DEV_NORMAL;
 
 		if( Sys_GetIntFromCmdLine( "-dev", &developer ))
-			developer = bound( DEV_NONE, developer, DEV_EXTENDED );
+			developer = bound( DEV_NONE, developer, 9 );
 	}
 
 #if XASH_ENGINE_TESTS
