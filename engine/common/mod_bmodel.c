2d8819915097bbc1812070b1178154e3414280ab
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-08 18:42:57 +0500

    engine: common: mod_bmodel: widen external BSP models clipnodes to 32-bit if the world is 32-bit

--- engine/common/mod_bmodel.c
+++ engine/common/mod_bmodel.c
@@ -137,6 +137,7 @@ typedef struct
 	int       version;          // model version
 	qboolean  isworld;
 	qboolean  isbsp30ext;
+	qboolean  hulls32bit;       // convert 16-bit clipnodes to 32-bit, if needed
 
 } dbspmodel_t;
 
@@ -1831,7 +1832,7 @@ static int RemapClipNodes_r( dbspmodel_t *bmod, dclipnode32_t *srcnodes, hull_t 
 		return nodenum;
 
 	// emit a clipnode
-	if( bmod->version == QBSP2_VERSION )
+	if( bmod->hulls32bit )
 	{
 		if( hull->lastclipnode == MAX_MAP_CLIPNODES_BSP2 )
 			Host_Error( "%s: MAX_MAP_CLIPNODES_BSP2 limit exceeded\n", __func__ );
@@ -1847,7 +1848,7 @@ static int RemapClipNodes_r( dbspmodel_t *bmod, dclipnode32_t *srcnodes, hull_t 
 	int c = hull->lastclipnode;
 	hull->lastclipnode++;
 
-	if( bmod->version == QBSP2_VERSION )
+	if( bmod->hulls32bit )
 	{
 		mclipnode32_t *out = &hull->clipnodes32[c];
 		out->planenum = src->planenum;
@@ -1880,7 +1881,7 @@ static void Mod_MakeHull0( model_t *mod, const dbspmodel_t *bmod )
 	hull->lastclipnode = mod->numnodes - 1;
 	hull->planes = mod->planes;
 
-	if( bmod->version == QBSP2_VERSION )
+	if( bmod->hulls32bit )
 	{
 		mclipnode32_t *out;
 		mnode_t *in = mod->nodes;
@@ -1981,7 +1982,7 @@ static void Mod_SetupHull( dbspmodel_t *bmod, model_t *mod, int headnode, int hu
 		// only allocate clipnodes array for the base model, only for first hull
 		if( mod == world && hullnum == 1 )
 		{
-			if( bmod->version == QBSP2_VERSION )
+			if( bmod->hulls32bit )
 			{
 				hull->clipnodes32 = Mem_Malloc( world->mempool, sizeof( *hull->clipnodes32 ) * mod->numclipnodes );
 
@@ -2006,7 +2007,7 @@ static void Mod_SetupHull( dbspmodel_t *bmod, model_t *mod, int headnode, int hu
 		}
 		else
 		{
-			if( bmod->version == QBSP2_VERSION )
+			if( bmod->hulls32bit )
 				hull->clipnodes32 = world->hulls[1].clipnodes32;
 			else
 				hull->clipnodes16 = world->hulls[1].clipnodes16;
@@ -2019,7 +2020,7 @@ static void Mod_SetupHull( dbspmodel_t *bmod, model_t *mod, int headnode, int hu
 		return; // hull missed
 
 	// fit array to real count
-	if( bmod->version == QBSP2_VERSION )
+	if( bmod->hulls32bit )
 	{
 		CountDClipNodes_r( bmod->clipnodes_out, hull, headnode, MAX_MAP_CLIPNODES_BSP2 );
 		hull->clipnodes32 = Mem_Malloc( world->mempool, sizeof( *hull->clipnodes32 ) * hull->lastclipnode );
@@ -2125,7 +2126,7 @@ static void Mod_SetupSubmodels( model_t *mod, dbspmodel_t *bmod )
 		mod->hulls[0].lastclipnode = bm->headnode[0]; // need to be real count
 
 		// counting a real number of clipnodes per each submodel
-		if( bmod->version == QBSP2_VERSION )
+		if( bmod->hulls32bit )
 			CountClipNodes32_r( mod->hulls[0].clipnodes32, &mod->hulls[0], bm->headnode[0] );
 		else
 			CountClipNodes16_r( mod->hulls[0].clipnodes16, &mod->hulls[0], bm->headnode[0] );
@@ -4193,6 +4194,7 @@ static qboolean Mod_LoadBmodelLumps( model_t *mod, byte *mod_base, size_t buffer
 	}
 	bmod->isworld = isworld;
 	bmod->isbsp30ext = FBitSet( flags, LUMP_BSP30EXT );
+	bmod->hulls32bit = bmod->version == QBSP2_VERSION || ( !isworld && world.version == QBSP2_VERSION );
 	fs_offset_t bspx_header_offset = Mod_FindBSPX( mod_base, bufferlen );
 
 	// loading base lumps
