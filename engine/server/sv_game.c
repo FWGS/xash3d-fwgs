93fe79b750b15742d3b72701f794eb81376f24ff
Simon Slamka <simon.slamka@ongakken.com>
2026-08-16 16:52:11 +0000

    engine: support cross-compile for win64 with mingw64

--- engine/server/sv_game.c
+++ engine/server/sv_game.c
@@ -3274,11 +3274,11 @@ void SV_PrintStr64Stats_f( void )
 	Con_Printf( "====================\n" );
 	Con_Printf( "64 bit string pool statistics\n" );
 	Con_Printf( "====================\n" );
-	Con_Printf( "string array size: %lu\n", str64.maxstringarray );
-	Con_Printf( "total alloc %lu\n", str64.totalalloc );
-	Con_Printf( "maximum array usage: %lu\n", str64.maxalloc );
-	Con_Printf( "overflow counter: %lu\n", str64.numoverflows );
-	Con_Printf( "dup string counter: %lu\n", str64.numdups );
+	Con_Printf( "string array size: %zu\n", str64.maxstringarray );
+	Con_Printf( "total alloc %zu\n", str64.totalalloc );
+	Con_Printf( "maximum array usage: %zu\n", str64.maxalloc );
+	Con_Printf( "overflow counter: %zu\n", str64.numoverflows );
+	Con_Printf( "dup string counter: %zu\n", str64.numdups );
 #else // !XASH_64BIT
 	Con_Printf( "Not implemented\n" );
 #endif // !XASH_64BIT
