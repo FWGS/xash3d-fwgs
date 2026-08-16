FWGS/xash3d-fwgs

commit d391ef1747cd6f624f187306632f943b21c09652

public: crtlib: make COM_FixSlashes remove duplicate slashes

public/crtlib.h
@@ -331,13 +331,24 @@ static inline int Q_splitstr( char *str, int delim, void *userdata,
 ============
 COM_FixSlashes
 
-Changes all '\' characters into '/' characters, in place.
+Changes all '\' characters into '/' characters and removes duplicate slashes, in place.
 ============
 */
 static inline void COM_FixSlashes( char *pname )
 {
-	while(( pname = Q_strchr( pname, '\\' )))
-		*pname = '/';
+	char *s = pname;
+	int i, j;
+
+	while(( s = Q_strchr( s, '\\' )))
+		*s = '/';
+
+	for( i = 0, j = 0; pname[i]; i++ )
+	{
+		if( pname[i] == '/' && pname[i+1] == '/' )
+			continue;
+		pname[j++] = pname[i];
+	}
+	pname[j] = 0;
 }
 
 #ifdef __cplusplus

commit bc24058050767e5eddad8cd5a8b02c6dc591de02

public: crtlib: preserve the leading double slash in COM_FixSlashes so that UNC paths are not modified
Fixes: d391ef17 ("public: crtlib: make COM_FixSlashes remove duplicate slashes")

public/crtlib.h
@@ -380,7 +380,10 @@ static inline void COM_FixSlashes( char *pname )
 	while(( s = Q_strchr( s, '\\' )))
 		*s = '/';
 
-	for( i = 0, j = 0; pname[i]; i++ )
+	// a leading '//' is an UNC path prefix and must be preserved
+	i = j = ( pname[0] == '/' && pname[1] == '/' ) ? 2 : 0;
+
+	for( ; pname[i]; i++ )
 	{
 		if( pname[i] == '/' && pname[i+1] == '/' )
 			continue;

public/tests/test_strings.c
@@ -51,6 +51,8 @@ static int Test_FixSlashes( void )
 	string s = "path\\with\\back\\slashes";
 	string s2 = "path/with/fwd/slashes";
 	string s3 = "path\\with/mixed\\slashes";
+	string s4 = "path//with///duplicate\\\\slashes";
+	string s5 = "\\\\server\\share\\path";
 
 	COM_FixSlashes( s );
 
@@ -66,6 +68,16 @@ static int Test_FixSlashes( void )
 
 	if( Q_strcmp( s3, "path/with/mixed/slashes" ))
 		return 3;
+
+	COM_FixSlashes( s4 );
+
+	if( Q_strcmp( s4, "path/with/duplicate/slashes" ))
+		return 4;
+
+	COM_FixSlashes( s5 );
+
+	if( Q_strcmp( s5, "//server/share/path" ))
+		return 5;
 
 	return 0;
 }