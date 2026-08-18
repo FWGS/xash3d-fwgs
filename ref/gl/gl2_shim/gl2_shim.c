ed2239e6138501c03a80bae95f4988c717cb2a64
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-18 19:16:53 +0500

    ref: gl: gl2_shim: another fix for inclusive start/end in DrawRangeElements

--- ref/gl/gl2_shim/gl2_shim.c
+++ ref/gl/gl2_shim/gl2_shim.c
@@ -1467,7 +1467,7 @@ static void GL2_UploadBufferData( gl2wrap_prog_t *prog, int size, GLuint start, 
 	}
 	rpglBindBufferARB( GL_ARRAY_BUFFER_ARB, gl2wrap_arrays.ptr[attr].vbo_fb[gl2wrap_arrays.ptr[attr].vbo_cycle] );
 	gl2wrap_arrays.ptr[attr].vbo_cycle = ( gl2wrap_arrays.ptr[attr].vbo_cycle + 1 ) % gl2wrap_config.cycle_buffers;
-	pglBufferDataARB( GL_ARRAY_BUFFER_ARB, end * stride, gl2wrap_arrays.ptr[attr].userptr, GL_STREAM_DRAW_ARB );
+	pglBufferDataARB( GL_ARRAY_BUFFER_ARB, ( end + 1 ) * stride, gl2wrap_arrays.ptr[attr].userptr, GL_STREAM_DRAW_ARB );
 	pglVertexAttribPointerARB( prog->attridx[attr], gl2wrap_arrays.ptr[attr].size, gl2wrap_arrays.ptr[attr].type, attr == GL2_ATTR_COLOR, gl2wrap_arrays.ptr[attr].stride, 0 );
 }
 /*
@@ -1489,7 +1489,7 @@ static void GL2_UpdatePersistentArrayBuffer( gl2wrap_prog_t *prog, int size, int
 		gl2wrap_arrays.stream_pointer = pglMapBufferRange( GL_ARRAY_BUFFER_ARB, 0, GL2_MAX_VERTS * 64, flags );
 		//i = -1;
 		//continue;
-		size = end * stride, offset = 0;
+		size = ( end + 1 ) * stride, offset = 0;
 	}
 
 	memcpy(((char *)gl2wrap_arrays.stream_pointer ) + gl2wrap_arrays.stream_counter, ((char *)gl2wrap_arrays.ptr[attr].userptr ) + offset, size );
@@ -1514,7 +1514,7 @@ static void GL2_UpdateIncrementalArrayBuffer( gl2wrap_prog_t *prog, int size, in
 
 	if( gl2wrap_arrays.stream_counter + size > GL2_MAX_VERTS * 64 )
 	{
-		size = end * stride;
+		size = ( end + 1 ) * stride;
 		offset = 0;
 		gl2wrap_arrays.stream_counter = 0;
 		inv = true;
@@ -1639,13 +1639,13 @@ static void GL2_SetupArrays( GLuint start, GLuint end )
 					gEngfuncs.Con_Printf( S_ERROR "NON-vbo array for DrawElements call, SKIPPING!\n" );
 					continue;
 				}
-				int size = ( end - start ) * stride;
+				int size = ( end - start + 1 ) * stride;
 				int offset = start * stride;
 
 				// Logical buffer start can lie before real buffer start
 				// but attrib pointer cannot have negative buffer offset
 				if( gl2wrap_arrays.stream_counter < offset )
-					size = end * stride, offset = 0;
+					size = ( end + 1 ) * stride, offset = 0;
 
 				if(( !gl2wrap_config.buf_storage && !gl2wrap_config.incremental ) || size > GL2_MAX_VERTS * 32 )
 				{
@@ -1697,7 +1697,11 @@ static void APIENTRY GL2_DrawRangeElements( GLenum mode, GLuint start, GLuint en
 
 static void APIENTRY GL2_DrawArrays( GLenum mode, GLint first, GLsizei count )
 {
-	GL2_SetupArrays( 0, count );
+	if( count <= 0 )
+		return;
+
+	// vertices referenced are [first, first + count - 1], and first may be nonzero
+	GL2_SetupArrays( 0, first + count - 1 );
 	rpglDrawArrays( mode, first, count );
 }
 
